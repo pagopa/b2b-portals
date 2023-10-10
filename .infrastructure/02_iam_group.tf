b2331a2906c037a2464160c141ee0d5d27dbffe6
Marco Comi <9998393+kin0992@users.noreply.github.com>
2023-10-10 08:51:22 +0200

    Use group policy attachment

--- .infrastructure/02_iam_group.tf
+++ .infrastructure/02_iam_group.tf
@@ -3,32 +3,6 @@ resource "aws_iam_group" "external_developers_read_only" {
   name = "external_developers_read_only"
 }
 
-# IAM Group Policy Read Only
-resource "aws_iam_group_policy" "external_developers" {
-  name  = "external_developers_policy"
-  group = aws_iam_group.external_developers_read_only.name
-
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Effect" : "Allow",
-        "Action" : [
-          # S3
-          "s3:ListBucket",
-          "s3:GetObject*"
-        ],
-        "Resource" : [
-          # Here we are going to list the S3 buckets
-          "*"
-        ]
-      }
-    ]
-  })
-}
-
 # IAM Group Membership - DGS
 resource "aws_iam_group_membership" "dgs" {
   name = "DGS"
@@ -38,4 +12,10 @@ resource "aws_iam_group_membership" "dgs" {
   ]
 
   group = aws_iam_group.external_developers_read_only.name
+}
+
+resource "aws_iam_group_policy_attachment" "read_only" {
+  group      = aws_iam_group.external_developers_read_only.name
+  # The AWS ReadOnly Access Policy
+  policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
 }
