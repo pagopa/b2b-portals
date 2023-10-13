e5f20e0b1124dc9d099cf9337036da91a12e699f
maurodandrea <98483756+maurodandrea@users.noreply.github.com>
2023-10-13 14:33:30 +0200

    Update .infrastructure/03_s3.tf
    Co-authored-by: AF <4775679+datalek@users.noreply.github.com>

--- .infrastructure/03_s3.tf
+++ .infrastructure/03_s3.tf
@@ -4,7 +4,7 @@ locals {
   account_id = data.aws_caller_identity.current.account_id
 }
 
-resource "aws_s3_bucket" "website-asset-bucket" {
+resource "aws_s3_bucket" "cms_medialibrary_bucket" {
   bucket = "media-library-s3-strapi-${local.account_id}"
 }
 
