715f1a6b5e74571c5b726bb1c58eebe0228a6745
maurodandrea <98483756+maurodandrea@users.noreply.github.com>
2023-10-16 15:38:34 +0200

    use-random-integer-for-bucket-id
    use-random-integer-for-bucket-id

--- .infrastructure/03_s3.tf
+++ .infrastructure/03_s3.tf
@@ -1,14 +1,13 @@
-data "aws_caller_identity" "current" {}
-
-locals {
-  account_id = data.aws_caller_identity.current.account_id
-}
-
-resource "aws_s3_bucket" "cms_medialibrary_bucket" {
-  bucket = "cms-medialibrary-${local.account_id}"
+resource "random_integer" "bucket_random_integer" {
+  min = 1
+  max = 9999
 }
 
 ### Bucket S3 for Media Library Strapi ###
+
+resource "aws_s3_bucket" "cms_medialibrary_bucket" {
+  bucket = "cms-medialibrary-${random_integer.bucket_random_integer.result}"
+}
 
 resource "aws_s3_bucket_public_access_block" "cms_medialibrary_bucket" {
   bucket                  = aws_s3_bucket.cms_medialibrary_bucket.id
