7e8dcd72384e2eb92651734c6b4021b9e23914e6
maurodandrea <98483756+maurodandrea@users.noreply.github.com>
2023-10-18 14:32:58 +0200

    add-ecs-ecr-alb-iam-resource
    Add ECS ECR ALB and IAM resource

---
+++ .infrastructure/09_ecs.tf
@@ -0,0 +1,55 @@
+resource "aws_ecs_cluster" "cms_ecs_cluster" {
+  name = var.ecs_cluster
+}
+
+data "template_file" "cms_app" {
+  template = file("./task-definitions/cms_app.json.tpl")
+
+  vars = {
+    image           = aws_ecr_repository.image_repository.repository_url
+    fargate_cpu     = var.fargate_cpu
+    fargate_memory  = var.fargate_memory
+    aws_region      = var.aws_region
+    db_host         = aws_rds_cluster.cms_database_cluster.endpoint
+    db_user         = aws_rds_cluster.cms_database_cluster.master_username
+    db_password_arn = aws_ssm_parameter.cms_database_password.arn
+    bucket_name     = aws_s3_bucket.cms_medialibrary_bucket.bucket
+    #github_token_arn     = aws_ssm_parameter.github_token.arn
+    admin_jwt_secret_arn = aws_ssm_parameter.cms_admin_jwt_secret.arn
+    db_name              = aws_rds_cluster.cms_database_cluster.database_name
+    db_client            = var.db_client
+  }
+}
+
+resource "aws_ecs_task_definition" "cms_task_def" {
+  family                   = var.ecs_task_def
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  task_role_arn            = aws_iam_role.task_role.arn
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = var.fargate_cpu
+  memory                   = var.fargate_memory
+  container_definitions    = data.template_file.cms_app.rendered
+}
+
+resource "aws_ecs_service" "cms_ecs_service" {
+  name                              = var.ecs_serv
+  cluster                           = var.ecs_cluster
+  desired_count                     = 1
+  launch_type                       = "FARGATE"
+  force_new_deployment              = true
+  task_definition                   = aws_ecs_task_definition.cms_task_def.arn
+  health_check_grace_period_seconds = 60000
+
+  network_configuration {
+    security_groups  = [aws_security_group.ecs_tasks.id]
+    subnets          = aws_subnet.cms_private.*.id
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_alb_target_group.app.id
+    container_name   = var.container_name
+    container_port   = var.app_port
+  }
+}
