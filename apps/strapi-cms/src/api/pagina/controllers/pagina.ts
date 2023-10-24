401e1aa186abbb3731b45bdbaa8b25a194ee872f
Simone Salsi <simone.salsi@dgsspa.com>
2023-10-24 14:17:47 +0200

    Created pagina containing Nome and slug

---
+++ apps/strapi-cms/src/api/pagina/controllers/pagina.ts
@@ -0,0 +1,7 @@
+/**
+ * pagina controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::pagina.pagina');
---
+++ apps/strapi-cms/src/api/pagina/routes/pagina.ts
@@ -0,0 +1,7 @@
+/**
+ * pagina router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::pagina.pagina');
