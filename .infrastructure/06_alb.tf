45b298bba93c69d628c903840c2d92bb889b6820
maurodandrea <98483756+maurodandrea@users.noreply.github.com>
2023-10-26 11:44:19 +0200

    remove-tfsecingore

--- .infrastructure/06_alb.tf
+++ .infrastructure/06_alb.tf
@@ -2,20 +2,20 @@ resource "aws_alb" "cms_load_balancer" {
   name            = var.cms_alb
   subnets         = module.vpc.public_subnets
   security_groups = [aws_security_group.cms_lb.id]
-  internal        = false #tfsec:ignore:AWS005
+  internal        = false
 }
 
 resource "aws_alb_target_group" "app" {
   name        = var.cms_alb_target_group
-  port        = var.app_port
-  protocol    = "HTTP" #tfsec:ignore:AWS004 - uses plain HTTP instead of HTTPS
+  port        = var.cms_app_port
+  protocol    = "HTTP"
   vpc_id      = module.vpc.vpc_id
   target_type = "ip"
 
   health_check {
     healthy_threshold   = "3"
     interval            = "30"
-    protocol            = "HTTP" #tfsec:ignore:AWS004 - uses plain HTTP instead of HTTPS
+    protocol            = "HTTP"
     matcher             = "200"
     timeout             = "3"
     path                = var.health_check_path
@@ -27,7 +27,7 @@ resource "aws_alb_target_group" "app" {
 resource "aws_alb_listener" "front_end" {
   load_balancer_arn = aws_alb.cms_load_balancer.id
   port              = 1337
-  protocol          = "HTTP" #tfsec:ignore:AWS004 - uses plain HTTP instead of HTTPS
+  protocol          = "HTTP"
 
   default_action {
     target_group_arn = aws_alb_target_group.app.id
