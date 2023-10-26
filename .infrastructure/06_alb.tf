c599b8efae723b56c63468a712f43a77a8d3abe7
maurodandrea <98483756+maurodandrea@users.noreply.github.com>
2023-10-26 17:15:50 +0200

    add-cloudfront

--- .infrastructure/06_alb.tf
+++ .infrastructure/06_alb.tf
@@ -33,3 +33,46 @@ resource "aws_alb_listener" "front_end" {
     type             = "forward"
   }
 }
+
+locals {
+alb_origin_id = aws_alb.cms_load_balancer.id
+}
+
+### AWS CloudFront distribution ###
+resource "aws_cloudfront_distribution" "cms_dist" {
+  enabled             = true
+  origin {
+    domain_name = aws_alb.cms_load_balancer.dns_name
+	  origin_id   = aws_alb.cms_load_balancer.dns_name
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+  
+  default_cache_behavior {
+    allowed_methods        = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
+    cached_methods         = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id       = aws_alb.cms_load_balancer.dns_name
+    viewer_protocol_policy = "redirect-to-https" # other options - https only, http
+    forwarded_values {
+      query_string = false
+      headers      = []
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+  
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
