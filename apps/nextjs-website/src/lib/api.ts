94181bc96ed78c98351472b9ba183edf4eda9293
Simone Salsi <simone.salsi@dgsspa.com>
2023-10-26 12:30:18 +0200

    Updated getAllPages to return title and visible

--- apps/nextjs-website/src/lib/api.ts
+++ apps/nextjs-website/src/lib/api.ts
@@ -27,7 +27,7 @@ interface Page {
 
 export const getAllPages = async () => {
   // Step 1: Fetch the navigation ID
-  const navigationApiUrl: string = 'http://localhost:1337/api/navigation';
+  const navigationApiUrl: string = 'http://127.0.0.1:1337/api/navigation';
   const token: string =
     'a7cc986b110df0aef1d499c2fbcdce5e6e5ca567127150a2dc00e2549c7b7960e73ede3425d676f04526e033a6f1227a9c1e0fa2269d4ff564a13ab52ce6ba8fce58d7d8b1298c42f1472ed7b83049b561c1c6e52b739a30dde44fd51bb3b8844b3e1ec35304e910a45dc2eaa0cd4d4a6486ebeb94a6078a78926c710866ed05';
 
@@ -56,7 +56,7 @@ export const getAllPages = async () => {
     const navigationId = navigationData[0].id;
 
     // Step 2: Fetch the page data using the navigation ID
-    const navigationRenderApiUrl: string = `http://localhost:1337/api/navigation/render/${navigationId}`;
+    const navigationRenderApiUrl: string = `http://127.0.0.1:1337/api/navigation/render/${navigationId}`;
 
     const response = await fetch(navigationRenderApiUrl, {
       method: 'GET',
@@ -73,7 +73,7 @@ export const getAllPages = async () => {
 
     const pages: Page[] = await response.json();
 
-    const slugs: { slug: string[] }[] = [];
+    const slugs: { slug: string[], title: string, visible: boolean }[] = [];
 
     pages.forEach((page: Page) => {
       // Ignore any case in which the slug is null or the special 'home' case
@@ -90,6 +90,8 @@ export const getAllPages = async () => {
       if (page.parent == null) {
         slugs.push({
           slug: pageSlug,
+          title: page.title,
+          visible: page.menuAttached ?? false
         });
       } else {
         // Build the slug of the page with a parent (max 5 levels deep)
@@ -110,7 +112,11 @@ export const getAllPages = async () => {
           if (pageParent.parent != null) {
             currPage = pageParent;
           } else {
-            slugs.push({ slug: pageSlug });
+            slugs.push({
+              slug: pageSlug,
+              title: page.title,
+              visible: page.menuAttached ?? false
+            });
             break;
           }
         }
@@ -119,9 +125,8 @@ export const getAllPages = async () => {
 
     // Filter the slugs
     const filteredSlugs = slugs
-      .filter((slugData) => slugData.slug != null)
-      .map((slugData) => ({ slug: slugData.slug }));
-
+      .filter((slugData) => slugData.slug != null);
+      // .map((slugData) => ({ slug: slugData.slug }));
     return {
       pages: filteredSlugs, // Use the filtered slugs instead of the original slugs
     };
