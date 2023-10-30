0ca30db90f9dba6b22ba10a68719f831c5406c7c
Simone Salsi <simone.salsi@dgsspa.com>
2023-10-30 17:36:07 +0100

    Installed AWS S3 plugin, added middleware configuration shape

--- apps/strapi-cms/config/middlewares.ts
+++ apps/strapi-cms/config/middlewares.ts
@@ -1,6 +1,31 @@
 export default [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'yourBucketName.s3.yourRegion.amazonaws.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'yourBucketName.s3.yourRegion.amazonaws.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::query',
   'strapi::body',
