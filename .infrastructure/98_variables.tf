c2d241e7290caead51189da9e56d3d0b8744c16b
maurodandrea <98483756+maurodandrea@users.noreply.github.com>
2023-10-30 11:33:14 +0100

    Apply suggestions from code review
    Modify comment cpu and ram
    
    Co-authored-by: Marco Comi <9998393+kin0992@users.noreply.github.com>

--- .infrastructure/98_variables.tf
+++ .infrastructure/98_variables.tf
@@ -28,10 +28,10 @@ variable "cms_app_port" {
 
 variable "cms_app_cpu" {
   description = "Fargate instance CPU units to provision (1 vCPU = 1024 CPU units)"
-  default     = "1024" ##### 4 vCPU
+  default     = "1024" ##### 1 vCPU
 }
 
 variable "cms_app_memory" {
   description = "Fargate instance memory to provision (in MiB)"
-  default     = "3072" ##### 16 GB RAM
+  default     = "3072" ##### 3 GB RAM
 }
