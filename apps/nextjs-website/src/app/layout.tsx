8b26ada3400ada2eb874bd877c8fce0a6ab126e6
Meolocious <90854445+Meolocious@users.noreply.github.com>
2023-10-31 16:38:47 +0100

    commit changes for PR

--- apps/nextjs-website/src/app/layout.tsx
+++ apps/nextjs-website/src/app/layout.tsx
@@ -1,12 +1,41 @@
 'use client';
+import React, { useState, useEffect } from 'react';
 import type { Metadata } from 'next';
 import { PreHeader } from '@pagopa/pagopa-editorial-components';
-import preHeaderData from '../lib/temporanyData/preHeaderData.json';
-import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+import { getPreHeaderData } from '@/lib/api';
 
 export const metadata: Metadata = {
   title: 'Page',
   description: 'New Page Created',
+};
+
+const fetchPreHeaderData = async (setPreHeaderData: Function) => {
+  const { preHeaderData } = await getPreHeaderData();
+
+  setPreHeaderData({
+    leftCtas: {
+      theme: preHeaderData.data.attributes.theme.toLowerCase(),
+      ctaButtons: [
+        {
+          text: preHeaderData.data.attributes.leftCTAButton.text,
+          variant: preHeaderData.data.attributes.leftCTAButton.variant,
+          color: preHeaderData.data.attributes.leftCTAButton.color,
+          href: preHeaderData.data.attributes.leftCTAButton.href,
+        },
+      ],
+    },
+    rightCtas: {
+      theme: preHeaderData.data.attributes.theme.toLowerCase(),
+      ctaButtons: [
+        {
+          text: preHeaderData.data.attributes.rightCTAButton.text,
+          variant: preHeaderData.data.attributes.rightCTAButton.variant,
+          color: preHeaderData.data.attributes.rightCTAButton.color,
+          href: preHeaderData.data.attributes.rightCTAButton.href,
+        },
+      ],
+    },
+  });
 };
 
 export default function RootLayout({
@@ -14,42 +43,30 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <html>
-      <body>
-        <PreHeader
-          leftCtas={{
-            ctaButtons: [
-              {
-                color: 'inherit',
-                href: '#',
-                text: 'Link',
-                variant: 'text',
-              },
-            ],
-            theme: 'light',
-          }}
-          rightCtas={{
-            ctaButtons: [
-              {
-                color: 'inherit',
-                onClick: function noRefCheck() {},
-                startIcon: <HelpOutlineIcon />,
-                text: 'Assistenza',
-                variant: 'text',
-              },
-            ],
-            theme: 'light',
-          }}
-        />
+  const [preHeaderData, setPreHeaderData] = useState<any>(null);
 
-        <PreHeader
-          leftCtas={preHeaderData.leftCtas}
-          rightCtas={preHeaderData.rightCtas}
-        />
+  useEffect(() => {
+    fetchPreHeaderData(setPreHeaderData);
+  }, []);
+
+  return (
+
+    <html>
+
+      <body>
+
+        {preHeaderData != null && (
+          <PreHeader
+            leftCtas={preHeaderData.leftCtas}
+            rightCtas={preHeaderData.rightCtas}
+          />
+        )}
 
         {children}
+
       </body>
+
     </html>
+
   );
 }
--- apps/nextjs-website/src/lib/api.ts
+++ apps/nextjs-website/src/lib/api.ts
@@ -1,12 +1,149 @@
-/** This file contains all the functions useful to get data from external resources */
+interface NavigationItem {
+  readonly order: number;
+  readonly id: number;
+  readonly title: string;
+  readonly type: string;
+  readonly path: string;
+  readonly externalPath: string | null;
+  readonly uiRouterKey: string;
+  readonly menuAttached: boolean;
+  readonly collapsed: boolean;
+  readonly createdAt: string;
+  readonly updatedAt: string;
+  readonly parent: NavigationItem | null;
+  readonly related: {
+    readonly id: number;
+    readonly Nome: string;
+    readonly slug: string;
+    readonly createdAt: string;
+    readonly updatedAt: string;
+    readonly publishedAt: string;
+    readonly __contentType: string;
+    readonly navigationItemId: number;
+    readonly createdBy: string;
+    readonly updatedBy: string;
+  };
+}
 
-// Return all the pages
-export const getAllPages = async () => ({
-  // for now hardcoded, then retrieved from the cms
-  pages: [
-    { slug: ['a'] },
-    { slug: ['a', 'b'] },
-    { slug: ['a', 'b', 'c'] },
-    { slug: ['a', 'b', 'c', 'd'] },
-  ],
-});
+export interface Page {
+  readonly id: number;
+  readonly title: string;
+  readonly slug: ReadonlyArray<string>;
+  readonly visible: boolean;
+}
+
+const GeneratePagesFromNavItems = (
+  navItems: ReadonlyArray<NavigationItem>
+): ReadonlyArray<Page> => {
+  const isEmptyOrHomePage = (navItem: NavigationItem) =>
+    navItem.related.slug == null ||
+    navItem.related.slug.toLowerCase().trim() === 'home';
+
+  const getParentSlugsChain = (
+    navItem: NavigationItem,
+    chain: ReadonlyArray<string> = []
+  ): ReadonlyArray<string> => {
+    if (!navItem.parent) {
+      return chain;
+    } else {
+      const parent = navItems.find((p) => p.id === navItem.parent?.id);
+      if (!parent) {
+        return chain;
+      }
+      return getParentSlugsChain(parent, [parent.related.slug, ...chain]);
+    }
+  };
+
+  const pages: ReadonlyArray<Page> = navItems
+    .filter((navItem) => !isEmptyOrHomePage(navItem))
+    .map((navItem) => {
+      const pageSlug = [navItem.related.slug, ...getParentSlugsChain(navItem)];
+
+      return {
+        id: navItem.id,
+        title: navItem.title,
+        slug: pageSlug,
+        visible: navItem.menuAttached ?? false,
+      };
+    });
+
+  return pages.filter((p) => p.slug.join('') !== '');
+};
+
+export const getAllPages = async () => {
+  // Step 1: Fetch the navigation ID
+  const token = process.env['NEXT_STRAPI_API_TOKEN'];
+  const apiBaseUrl = process.env['NEXT_PUBLIC_API_BASE_URL'];
+  const navigationApiPath = '/api/navigation';
+  const navigationApiUrl = `${apiBaseUrl}${navigationApiPath}`;
+
+  const navigationResponse = await fetch(navigationApiUrl, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  if (!navigationResponse.ok) {
+    return {
+      error: 'Failed to fetch navigation data from the API',
+    };
+  }
+
+  const navigationData = await navigationResponse.json();
+
+  if (navigationData.length === 0) {
+    return {
+      error: 'No navigation data found',
+    };
+  }
+
+  const navigationId = navigationData[0].id;
+
+  // Step 2: Fetch the page data using the navigation ID
+  const navigationRenderApiUrl: string = `${apiBaseUrl}${navigationApiPath}/render/${navigationId}`;
+
+  const response = await fetch(navigationRenderApiUrl, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  if (!response.ok) {
+    return {
+      error: 'Failed to fetch data from the API',
+    };
+  }
+
+  const navItems: ReadonlyArray<NavigationItem> = await response.json();
+
+  const pages: ReadonlyArray<Page> = GeneratePagesFromNavItems(navItems);
+
+  return { pages };
+};
+
+export const getPreHeaderData = async () => {
+  const token = process.env['NEXT_STRAPI_API_TOKEN'];
+  const apiBaseUrl = process.env['NEXT_PUBLIC_API_BASE_URL'];
+  const preHeaderApiUrl: string = `${apiBaseUrl}/api/pre-header/?populate=*`;
+
+  const preHeaderResponse = await fetch(preHeaderApiUrl, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  if (!preHeaderResponse.ok) {
+    return {
+      error: 'Failed to fetch pre-header data',
+      preHeaderData: null,
+    };
+  }
+
+  return {
+    preHeaderData: await preHeaderResponse.json(),
+    error: null, // No error occurred
+  };
+};
