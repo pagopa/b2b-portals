cf879c6dcf90c6673cd29e694e03a07d3c74571c
Meolocious <90854445+Meolocious@users.noreply.github.com>
2023-11-06 11:57:27 +0100

    next.config changes for build generation

--- apps/nextjs-website/next.config.js
+++ apps/nextjs-website/next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export',
+  distDir: 'dist',
+  trailingSlash: true,
+}
 
 module.exports = nextConfig
