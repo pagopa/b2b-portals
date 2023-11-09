93d8459e3c47dac7c87f8c782c696172399a5985
maurodandrea <98483756+maurodandrea@users.noreply.github.com>
2023-11-09 14:40:55 +0100

    add-role-for-deploy-cms

--- .infrastructure/09_iam_role_deploy.tf
+++ .infrastructure/09_iam_role_deploy.tf
@@ -1,12 +1,11 @@
 data "aws_caller_identity" "current" {}
 
 ###############################################################################
-#                      Define IAM Role to use on deploy                       #
+#                      IAM Role to use on deploy website                       #
 ###############################################################################
 resource "aws_iam_role" "deploy_website" {
   name        = "GitHubActionDeployWebsite"
   description = "Role to assume to deploy the website"
-
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -75,4 +74,76 @@ resource "aws_iam_policy" "deploy_website" {
 resource "aws_iam_role_policy_attachment" "deploy_website" {
   role       = aws_iam_role.deploy_website.name
   policy_arn = aws_iam_policy.deploy_website.arn
+}
+
+###############################################################################
+#                      IAM Role to use on deploy CMS                       #
+###############################################################################
+resource "aws_iam_role" "deploy_ecs" {
+  name        = "GitHubActionDeployECS"
+  description = "Role to assume to deploy on ECS."
+
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          "Federated" : "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"
+        },
+        Action = "sts:AssumeRoleWithWebIdentity",
+        Condition = {
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" : "repo:${var.github_repository}:*"
+          },
+          "ForAllValues:StringEquals" = {
+            "token.actions.githubusercontent.com:iss" : "https://token.actions.githubusercontent.com",
+            "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "deploy_ecs" {
+  name        = "PagoPaECSDeploy"
+  description = "Policy to allow deploy on ECS."
+
+  policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Action" : [
+            "ecs:DescribeTaskDefinition",
+            "ecs:RegisterTaskDefinition",
+            "ecs:DescribeServices",
+            "ecs:UpdateService"
+          ],
+          "Effect" : "Allow",
+          "Resource" : [
+            "*"
+          ],
+          "Sid" : "ECSDeploy"
+        },
+        {
+          "Action" : [
+            "iam:PassRole"
+          ],
+          "Effect" : "Allow",
+          "Resource" : [
+            "${aws_iam_role.ecs_task_execution_role.arn}",
+            "${aws_iam_role.task_role.arn}"
+          ]
+        }
+      ],
+    }
+  )
+}
+
+resource "aws_iam_role_policy_attachment" "deploy_ecs" {
+  role       = aws_iam_role.deploy_ecs.name
+  policy_arn = aws_iam_policy.deploy_ecs.arn
 }
