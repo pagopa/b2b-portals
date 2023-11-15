e54c94537ec8bec78027a9f22bd23630ec7af58b
Meolocious <90854445+Meolocious@users.noreply.github.com>
2023-11-15 15:06:46 +0100

    remove unusued data

--- apps/nextjs-website/src/components/HeaderClient.tsx
+++ apps/nextjs-website/src/components/HeaderClient.tsx
@@ -3,6 +3,6 @@ import React from 'react';
 import { Header } from '@pagopa/pagopa-editorial-components';
 import { HeaderProps } from '@pagopa/pagopa-editorial-components/dist/components/Header/Header';
 
-export const HeaderClient: React.FC<HeaderProps> = (headerData, menuData) => (
-  <Header {...headerData} {...menuData} />
+export const HeaderClient: React.FC<HeaderProps> = (headerData) => (
+  <Header {...headerData} />
 );
