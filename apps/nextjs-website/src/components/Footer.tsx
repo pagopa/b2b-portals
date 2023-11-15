7e72d91fac343d9535bfb309d4775ec8dceb191b
Meolocious <90854445+Meolocious@users.noreply.github.com>
2023-11-15 15:45:21 +0100

    commit refactoring branch

--- apps/nextjs-website/src/components/Footer.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-import React from 'react';
-import { Footer } from '@pagopa/pagopa-editorial-components';
-import { FooterProps } from '@pagopa/pagopa-editorial-components/dist/components/Footer';
-
-export const FooterClient: React.FC<FooterProps> = (footerData) => (
-  <Footer {...footerData} />
-);
