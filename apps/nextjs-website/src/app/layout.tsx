d2d12455b5d078a946461e5f877ab90fc05cba42
Meolocious <90854445+Meolocious@users.noreply.github.com>
2023-11-16 10:27:04 +0100

    commit removing preHeader and Header parts

--- apps/nextjs-website/src/app/layout.tsx
+++ apps/nextjs-website/src/app/layout.tsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import type { Metadata } from 'next';
-import { PreHeaderProps } from '@pagopa/pagopa-editorial-components/dist/components/PreHeader';
-import { HeaderProps } from '@pagopa/pagopa-editorial-components/dist/components/Header/Header';
 import { FooterProps } from '@pagopa/pagopa-editorial-components/dist/components/Footer';
-import { PreHeaderClient } from '@/components/PreHeaderClient';
-import { HeaderClient } from '@/components/HeaderClient';
-import { getPreHeaderData } from '@/lib/api';
-import { getHeaderData } from '@/lib/api';
 import { FooterClient } from '@/components/FooterClient';
 import { getFooterData } from '@/lib/api/footerAPI';
 
@@ -20,15 +14,11 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const preHeaderData: PreHeaderProps = await getPreHeaderData();
-  const headerData: HeaderProps = await getHeaderData();
   const footerData: FooterProps = await getFooterData();
 
   return (
     <html>
       <body>
-        <PreHeaderClient {...preHeaderData} />
-        <HeaderClient {...headerData} />
         {children}
         <FooterClient {...footerData} />
       </body>
--- apps/nextjs-website/src/components/HeaderClient.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-import React from 'react';
-import { Header } from '@pagopa/pagopa-editorial-components';
-import { HeaderProps } from '@pagopa/pagopa-editorial-components/dist/components/Header/Header';
-
-export const HeaderClient: React.FC<HeaderProps> = (headerData) => (
-  <Header {...headerData} />
-);
