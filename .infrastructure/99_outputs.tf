9b4e0cd8262edcb7bd46b4d978713702e63f9fc6
maurodandrea <98483756+maurodandrea@users.noreply.github.com>
2023-11-16 15:56:27 +0100

    output-dns_cloudfront_website

--- .infrastructure/99_outputs.tf
+++ .infrastructure/99_outputs.tf
@@ -5,3 +5,7 @@ output "terraform_backend_bucket_name" {
 output "terraform_lock_dynamodb_table" {
   value = module.identity.terraform_lock_dynamodb_table
 }
+
+output "dns_cloudfront_website" {
+  value = aws_cloudfront_distribution.website.domain_name
+}
