114d36d300f2bcc29405ff1d8851d087bec70e87
Simone Salsi <simone.salsi@dgsspa.com>
2023-11-17 17:39:44 +0100

    Added AWS_PROVIDER env var to allow local testing of Media Library

--- apps/strapi-cms/config/plugins.ts
+++ apps/strapi-cms/config/plugins.ts
@@ -1,29 +1,29 @@
 export default ({ env }: any) => ({
-    upload: {
-      config: {
-        provider: 'aws-s3',
-        providerOptions: {
-          accessKeyId: env('AWS_ACCESS_KEY_ID'),
-          secretAccessKey: env('AWS_ACCESS_SECRET'),
-          region: env('AWS_REGION'),
-          params: {
-            Bucket: env('AWS_BUCKET_NAME'),
-          },
+  upload: {
+    config: {
+      provider: env('AWS_PROVIDER', 'aws-s3'),
+      providerOptions: {
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_ACCESS_SECRET'),
+        region: env('AWS_REGION'),
+        params: {
+          Bucket: env('AWS_BUCKET_NAME'),
         },
       },
     },
-    navigation: {
-      enabled: true,
-      config: {
-        contentTypes: ['api::page.page'],
-        contentTypesNameFields: {
-            'api::page.page': ['name']
-        },
-        pathDefaultFields: {
-            'api::page.page': ['slug']
-        },
-        allowedLevels: 2,
-        cascadeMenuAttached: false
-      }
-    },
+  },
+  navigation: {
+    enabled: true,
+    config: {
+      contentTypes: ['api::page.page'],
+      contentTypesNameFields: {
+          'api::page.page': ['name']
+      },
+      pathDefaultFields: {
+          'api::page.page': ['slug']
+      },
+      allowedLevels: 2,
+      cascadeMenuAttached: false
+    }
+  },
 });
