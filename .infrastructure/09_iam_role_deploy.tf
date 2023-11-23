6764d092311d1797531b94905c9fdc848ac66d62
maurodandrea <98483756+maurodandrea@users.noreply.github.com>
2023-11-23 17:56:02 +0100

    Update .infrastructure/09_iam_role_deploy.tf
    Co-authored-by: Marco Comi <9998393+kin0992@users.noreply.github.com>

--- .infrastructure/09_iam_role_deploy.tf
+++ .infrastructure/09_iam_role_deploy.tf
@@ -8,6 +8,7 @@ data "aws_iam_policy_document" "deploy_github" {
       type        = "Federated"
       identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"]
     }
+    
     condition {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
