e18c90e38f9599888097851b4b05de0b36361844
Meolocious <90854445+Meolocious@users.noreply.github.com>
2023-11-27 17:00:19 +0100

    commit editorial branch creation

--- apps/nextjs-website/src/components/rendering/rendering.tsx
+++ apps/nextjs-website/src/components/rendering/rendering.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import { HeroProps } from '@pagopa/pagopa-editorial-components/dist/components/Hero';
+import { EditorialProps } from '@pagopa/pagopa-editorial-components/dist/components/Editorial';
 import Hero from '../Hero';
-import { SectionDataToHeroProps } from './sectionDataToProps';
+import Editorial from '../Editorial';
+import { SectionDataToHeroProps, SectionDataToEditorialProps } from './sectionDataToProps';
 import { PageData } from '@/lib/fetch/page';
 
 export function rendering(
   componentData: PageData['data']['attributes']['sections'][0],
   index: number
 ) {
-  // Temporarily disable no-small-switch rule (new cases will be added with PRs for other components)
-  // eslint-disable-next-line no-underscore-dangle,sonarjs/no-small-switch
+  // eslint-disable-next-line no-underscore-dangle
   switch (componentData.__component) {
     case 'sections.hero':
       const HeroSectionProps: HeroProps = SectionDataToHeroProps(componentData);
       return <Hero key={index} {...HeroSectionProps} />; // id={componentData.sectionID}
+
+    case 'sections.editorial':
+      const EditorialSectionProps: EditorialProps =
+        SectionDataToEditorialProps(componentData);
+      return <Editorial key={index} {...EditorialSectionProps} />; // id={componentData.sectionID}
 
     default:
       return null;
--- apps/nextjs-website/src/components/rendering/sectionDataToProps.tsx
+++ apps/nextjs-website/src/components/rendering/sectionDataToProps.tsx
@@ -1,5 +1,6 @@
 import { HeroProps } from '@pagopa/pagopa-editorial-components/dist/components/Hero';
-import { HeroSectionData } from '@/lib/fetch/page';
+import { EditorialProps } from '@pagopa/pagopa-editorial-components/dist/components/Editorial';
+import { HeroSectionData, EditorialSectionData } from '@/lib/fetch/page';
 
 export const SectionDataToHeroProps = ({
   title,
@@ -20,4 +21,29 @@ export const SectionDataToHeroProps = ({
   inverse,
   background: background.data?.attributes.url,
   ctaButtons,
+});
+
+export const SectionDataToEditorialProps = ({
+  title,
+  eyelet,
+  body,
+  pattern,
+  width,
+  reversed,
+  image,
+  ctaButtons,
+}: EditorialSectionData): EditorialProps => ({
+  title,
+  ...(eyelet && { eyelet }),
+  body, // TODO: Parse rich text (markdown)
+  reversed,
+  width,
+  pattern,
+  ctaButtons,
+  image: (
+    <img
+      src={'http://localhost:1337' + (image.data?.attributes.url ?? '')} // TODO: Sub "http://localhost:1337" for config.STRAPI_API_BASE_URL
+      alt={image.data?.attributes.alternativeText ?? ''}
+    />
+  ),
 });
