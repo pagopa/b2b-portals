d7b9ddf15bc6e7dc0709c26cb2d860c3c0c5e18e
Meolocious <90854445+Meolocious@users.noreply.github.com>
2023-11-27 12:39:47 +0100

    [B2BP-179] - Fetch and render Header component (#78)

--- apps/nextjs-website/src/lib/api.ts
+++ apps/nextjs-website/src/lib/api.ts
@@ -1,9 +1,12 @@
 /** This file contains all the functions useful to get data from external resources */
 import { pipe } from 'fp-ts/lib/function';
 import * as E from 'fp-ts/lib/Either';
+import { HeaderProps } from '@pagopa/pagopa-editorial-components/dist/components/Header/Header';
 import { Page, makePageListFromNavigation } from './pages';
 import { getNavigation } from './fetch/navigation';
 import { PreHeader, getPreHeader } from './fetch/preHeader';
+import { getHeader } from './fetch/header';
+import { makeHeaderProps } from './header';
 import { makeAppEnv } from '@/AppEnv';
 
 // create AppEnv given process env
@@ -29,4 +32,10 @@ export const getPreHeaderProps = async (): Promise<
     data: { attributes },
   } = await getPreHeader(appEnv);
   return attributes;
+};
+
+export const getHeaderProps = async (): Promise<HeaderProps> => {
+  const header = await getHeader(appEnv);
+  const navigation = await getNavigation('main-navigation', appEnv);
+  return makeHeaderProps(navigation, header);
 };
---
+++ apps/nextjs-website/src/lib/fetch/__tests__/data.ts
@@ -0,0 +1,28 @@
+import { Navigation } from '../navigation';
+
+export const homepageNavItem: Navigation[0] = {
+  order: 1,
+  id: 1,
+  title: 'Home',
+  path: '/',
+  parent: null,
+  menuAttached: false,
+};
+
+export const parentNavItem: Navigation[0] = {
+  order: 2,
+  id: 2,
+  title: 'Parent',
+  path: 'parent',
+  parent: null,
+  menuAttached: true,
+};
+
+export const childNavItem: Navigation[0] = {
+  order: 3,
+  id: 3,
+  title: 'Child',
+  path: 'child',
+  parent: parentNavItem,
+  menuAttached: true,
+};
