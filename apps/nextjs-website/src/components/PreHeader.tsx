be3b5d59ed219557744b135566d0968cddd61dac
Simone Salsi <simone.salsi@dgsspa.com>
2023-11-28 18:31:34 +0100

    Moved PreHeaderProps refinement to PreHeader.tsx

--- apps/nextjs-website/src/components/PreHeader.tsx
+++ apps/nextjs-website/src/components/PreHeader.tsx
@@ -1,15 +1,66 @@
 'use client';
 import React from 'react';
-import {
-  PreHeaderProps,
-  PreHeader as PreHeaderEC,
-} from '@pagopa/pagopa-editorial-components/dist/components/PreHeader';
+import { PreHeader as PreHeaderEC } from '@pagopa/pagopa-editorial-components/dist/components/PreHeader';
 import '@/styles/preHeader.css';
+import Icon from '@mui/material/Icon';
+import * as MuiIcons from '@mui/icons-material';
+import { PreHeader } from '@/lib/fetch/preHeader';
 // TODO: Component styles are loaded client-side (even if we move the import to a server side component above this one). This causes a flash of the unstyled component (even when everything is rendered as static HTML) that needs to be fixed.
 
-const PreHeader: React.FC<PreHeaderProps> = (preHeaderData) => (
+const formatValidMuiIcon = (input?: string | null): string | null => {
+  if (!input || !(input in MuiIcons)) {
+    return null;
+  }
+
+  // Convert from camel case (anExample) or pascal case (AnExample) to snake case (an_example)
+  return input
+    .replace(/(?:^|\.?)([A-Z])/g, (_, y) => `_${y.toLowerCase()}`)
+    .split(/^_/)
+    .join('');
+};
+
+const RefinePreHeaderProps = (props: PreHeader['data']['attributes']) => ({
+  ...props,
+  ...(props.leftCtas?.ctaButtons && {
+    leftCtas: {
+      ...props.leftCtas,
+      ctaButtons: props.leftCtas.ctaButtons.map((ctaBtn) => ({
+        ...ctaBtn,
+        disableRipple: true,
+        disableTouchRipple: true,
+        target: '_blank',
+        rel: 'noopener noreferrer',
+        sx: {
+          fontWeight: 'bold',
+          letterSpacing: '0',
+        },
+      })),
+    },
+  }),
+  ...(props.rightCtas?.ctaButtons && {
+    rightCtas: {
+      ...props.rightCtas,
+      ctaButtons: props.rightCtas.ctaButtons.map((ctaBtn) => ({
+        ...ctaBtn,
+        disableRipple: true,
+        disableTouchRipple: true,
+        target: '_blank',
+        rel: 'noopener noreferrer',
+        sx: {
+          fontWeight: '600',
+          letterSpacing: '.3px',
+        },
+        startIcon: <Icon>{formatValidMuiIcon(ctaBtn.icon)}</Icon>,
+      })),
+    },
+  }),
+});
+
+const PreHeader: React.FC<PreHeader['data']['attributes']> = (
+  preHeaderData: PreHeader['data']['attributes']
+) => (
   <div id='pre-header'>
-    <PreHeaderEC {...preHeaderData} />
+    <PreHeaderEC {...RefinePreHeaderProps(preHeaderData)} />
   </div>
 );
 
--- apps/nextjs-website/src/lib/api.ts
+++ apps/nextjs-website/src/lib/api.ts
@@ -2,7 +2,6 @@
 import { pipe } from 'fp-ts/lib/function';
 import * as E from 'fp-ts/lib/Either';
 import { HeaderProps } from '@pagopa/pagopa-editorial-components/dist/components/Header/Header';
-import { HelpOutlineOutlined } from '@mui/icons-material';
 import { Page, makePageListFromNavigation } from './pages';
 import { getNavigation } from './fetch/navigation';
 import { PreHeader, getPreHeader } from './fetch/preHeader';
@@ -33,44 +32,7 @@ export const getPreHeaderProps = async (): Promise<
     data: { attributes },
   } = await getPreHeader(appEnv);
 
-  // Remove MUI ripple effect from buttons + Explicit target + Add rel for SEO + Add styling for specific elements
-  return {
-    ...attributes,
-    ...(attributes.leftCtas?.ctaButtons && {
-      leftCtas: {
-        ...attributes.leftCtas,
-        ctaButtons: attributes.leftCtas.ctaButtons.map((ctaBtn) => ({
-          ...ctaBtn,
-          disableRipple: true,
-          disableTouchRipple: true,
-          target: '_blank',
-          rel: 'noopener noreferrer',
-          sx: {
-            fontWeight: 'bold',
-            letterSpacing: '0',
-          },
-        })),
-      },
-    }),
-    ...(attributes.rightCtas?.ctaButtons && {
-      rightCtas: {
-        ...attributes.rightCtas,
-        ctaButtons: attributes.rightCtas.ctaButtons.map((ctaBtn) => ({
-          ...ctaBtn,
-          disableRipple: true,
-          disableTouchRipple: true,
-          target: '_blank',
-          rel: 'noopener noreferrer',
-          sx: {
-            fontWeight: '600',
-            letterSpacing: '.3px',
-          },
-          startIcon: <HelpOutlineOutlined />,
-          // TODO: Select startIcon based on ctaBtn.icon
-        })),
-      },
-    }),
-  };
+  return attributes;
 };
 
 export const getHeaderProps = async (): Promise<HeaderProps> => {
