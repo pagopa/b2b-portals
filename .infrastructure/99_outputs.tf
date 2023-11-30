d801502356b5070993d71171b1a92aeaeac8284f
maurodandrea <98483756+maurodandrea@users.noreply.github.com>
2023-11-30 11:23:09 +0100

    remove-output-dns-cloudfront
    remove-output-dns-cloudfront

--- .infrastructure/99_outputs.tf
+++ .infrastructure/99_outputs.tf
@@ -5,7 +5,3 @@ output "terraform_backend_bucket_name" {
 output "terraform_lock_dynamodb_table" {
   value = module.identity.terraform_lock_dynamodb_table
 }
-
-output "dns_cloudfront_website" {
-  value = aws_cloudfront_distribution.website.domain_name
-}
