740a3f8c913f9b6d7ff1872942883192a3b2119e
Simone Salsi <simone.salsi@dgsspa.com>
2023-12-06 14:32:45 +0100

    Switched to a JS-first approach

--- apps/nextjs-website/src/components/PreHeader.tsx
+++ apps/nextjs-website/src/components/PreHeader.tsx
@@ -6,6 +6,7 @@ import Icon from '@mui/material/Icon';
 // Only temporarily importing every icon. A task is planned to sub this for a restricted set of accepted icon names.
 import * as MuiIcons from '@mui/icons-material';
 import { PreHeader } from '@/lib/fetch/preHeader';
+import buttonStyles from '@/styles/sx/button.json';
 
 const isValidMuiIcon = (iconName?: string | null): boolean =>
   !!iconName && iconName in MuiIcons;
@@ -38,6 +39,9 @@ const RefinePreHeaderProps = (props: PreHeader['data']['attributes']) => ({
         sx: {
           fontWeight: 'bold',
           letterSpacing: '0',
+          ...buttonStyles.common,
+          ...(props.leftCtas &&
+            buttonStyles[ctaBtn.variant][props.leftCtas.theme]),
         },
         ...(isValidMuiIcon(ctaBtn.icon) && {
           startIcon: <Icon>{formatValidMuiIcon(ctaBtn.icon)}</Icon>,
@@ -57,6 +61,9 @@ const RefinePreHeaderProps = (props: PreHeader['data']['attributes']) => ({
         sx: {
           fontWeight: '600',
           letterSpacing: '.3px',
+          ...buttonStyles.common,
+          ...(props.rightCtas &&
+            buttonStyles[ctaBtn.variant][props.rightCtas.theme]),
         },
         ...(isValidMuiIcon(ctaBtn.icon) && {
           startIcon: <Icon>{formatValidMuiIcon(ctaBtn.icon)}</Icon>,
