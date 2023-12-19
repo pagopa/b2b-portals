08ff7ce09b997cee7a992aaebc7766bfc56ee5eb
Meolocious <90854445+Meolocious@users.noreply.github.com>
2023-12-19 13:16:58 +0100

    commit hero in stack

--- apps/nextjs-website/src/components/Hero.tsx
+++ apps/nextjs-website/src/components/Hero.tsx
@@ -2,13 +2,20 @@
 import React from 'react';
 import { Hero as HeroEC } from '@pagopa/pagopa-editorial-components';
 import { HeroProps } from '@pagopa/pagopa-editorial-components/dist/components/Hero/index';
+import { Stack } from '@mui/material';
 
 const Hero: React.FC<HeroProps & { sectionID: string | undefined }> = (
   HeroData
 ) => (
-  <section id={HeroData.sectionID}>
-    <HeroEC {...HeroData} />;
-  </section>
+  <Stack
+    sx={{
+      color: '#fff',
+    }}
+  >
+    <section id={HeroData.sectionID}>
+      <HeroEC {...HeroData} />;
+    </section>
+  </Stack>
 );
 
 export default Hero;
