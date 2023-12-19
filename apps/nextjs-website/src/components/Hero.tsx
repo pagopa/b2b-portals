41adbfcc593011c41819fee5b860e28bd1b01094
Meolocious <90854445+Meolocious@users.noreply.github.com>
2023-12-19 18:04:11 +0100

    commit mobile style fix

--- apps/nextjs-website/src/components/Hero.tsx
+++ apps/nextjs-website/src/components/Hero.tsx
@@ -55,17 +55,31 @@ const Hero: React.FC<ExtendedHeroProps & { sectionID: string | undefined }> = (
               ':first-of-type': {
                 padding: '0',
                 [theme.breakpoints.up('md')]: {
-                  padding: '0 2rem',
+                  padding: '0 0.5rem',
                 },
               },
             },
           },
           '.MuiBox-root': {
+            ':first-of-type': {
+              padding: '0 0.8rem',
+              [theme.breakpoints.up('md')]: {
+                padding: '0 0.5rem',
+              },
+            },
+          },
+          '.MuiContainer-root': {
             '.MuiGrid-root': {
-              ':first-of-type': {
-                padding: '0',
-                [theme.breakpoints.up('md')]: {
-                  padding: '0 2rem',
+              '.MuiGrid-root': {
+                '.MuiStack-root': {
+                  ':first-of-type': {
+                    margin: '0',
+                    padding: '2rem 0',
+                    [theme.breakpoints.up('md')]: {
+                      margin: '0',
+                      padding: '0',
+                    },
+                  },
                 },
               },
             },
