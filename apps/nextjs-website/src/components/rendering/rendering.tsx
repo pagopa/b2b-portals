9a83a7390924215b417d44c1fb520d3f44ab181b
Meolocious <90854445+Meolocious@users.noreply.github.com>
2023-12-20 13:38:05 +0100

    commit fix branch + editorial style check

--- apps/nextjs-website/src/components/rendering/rendering.tsx
+++ apps/nextjs-website/src/components/rendering/rendering.tsx
@@ -1,27 +1,12 @@
 import React from 'react';
-import { HeroProps } from '@pagopa/pagopa-editorial-components/dist/components/Hero';
 import { EditorialProps } from '@pagopa/pagopa-editorial-components/dist/components/Editorial';
-import Hero from '../Hero';
 import Editorial from '../Editorial';
-import {
-  SectionDataToHeroProps,
-  SectionDataToEditorialProps,
-} from './sectionDataToProps';
+import { SectionDataToEditorialProps } from './sectionDataToProps';
 import { PageSection } from '@/lib/fetch/types/PageSection';
 
 export function rendering(componentData: PageSection, index: number) {
   // eslint-disable-next-line no-underscore-dangle
   switch (componentData.__component) {
-    case 'sections.hero':
-      const HeroSectionProps: HeroProps = SectionDataToHeroProps(componentData);
-      return (
-        <Hero
-          key={index}
-          {...HeroSectionProps}
-          sectionID={componentData.sectionID ?? undefined}
-        />
-      );
-
     case 'sections.editorial':
       const EditorialSectionProps: EditorialProps =
         SectionDataToEditorialProps(componentData);
