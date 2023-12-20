6e2189928dcfa2bc389d703e17cfc559e385fb14
Meolocious <90854445+Meolocious@users.noreply.github.com>
2023-12-20 17:41:16 +0100

    commit remove hero parts from PR

--- apps/nextjs-website/src/components/PreHeader.tsx
+++ apps/nextjs-website/src/components/PreHeader.tsx
@@ -1,115 +1,12 @@
 'use client';
 import React from 'react';
 import {
-  PreHeader as PreHeaderEC,
   PreHeaderProps,
+  PreHeader as PreHeaderEC,
 } from '@pagopa/pagopa-editorial-components/dist/components/PreHeader';
-import Icon from '@mui/material/Icon';
-// Only temporarily importing every icon. A task is planned to sub this for a restricted set of accepted icon names.
-import * as MuiIcons from '@mui/icons-material';
-import { Stack } from '@mui/material';
-import { PreHeader } from '@/lib/fetch/preHeader';
 
-const isValidMuiIcon = (iconName?: string | null): boolean =>
-  !!iconName && iconName in MuiIcons;
-
-const formatValidMuiIcon = (iconName?: string | null): string | null => {
-  if (!iconName || !(iconName in MuiIcons)) {
-    // Not using isValidMuiIcon because typescript doesn't register iconName as non-null if we do
-    return null;
-  }
-
-  // Convert from camel case (anExample) or pascal case (AnExample) to snake case (an_example)
-  return iconName
-    .replace(/(?:^|\.?)([A-Z])/g, (_, y) => `_${y.toLowerCase()}`)
-    .split(/^_/)
-    .join('');
-};
-
-const preHeaderNakedButtonStyle = {
-  padding: '0',
-  color: 'text.primary', // Theme-aware property --> equivalent to (theme) => theme.palette.text.primary
-  backgroundColor: 'transparent',
-  '&:hover': {
-    backgroundColor: 'transparent',
-    color: 'text.secondary', // Theme-aware property --> equivalent to (theme) => theme.palette.text.secondary
-  },
-};
-
-// Add styles, SEO related values and extra JS parameters for singular components
-// Styling 'naked' variant for PreHeader using 'text' variant as a base
-// (since editorial-components does not accept 'naked' variant)
-const RefinePreHeaderProps = (props: PreHeader['data']['attributes']) => ({
-  ...props,
-  ...(props.leftCtas?.ctaButtons && {
-    leftCtas: {
-      ...props.leftCtas,
-      ctaButtons: props.leftCtas.ctaButtons.map((ctaBtn) => ({
-        ...ctaBtn,
-        target: '_blank',
-        rel: 'noopener noreferrer',
-        disableRipple: ctaBtn.variant === 'text', // To be replaced by 'naked' when implemented on Strapi's side
-        disableTouchRipple: ctaBtn.variant === 'text', // To be replaced by 'naked' when implemented on Strapi's side
-        variant: ctaBtn.variant === 'text' ? 'text' : ctaBtn.variant, // To be replaced by 'naked' when implemented on Strapi's side
-        sx: {
-          fontWeight: 'bold',
-          letterSpacing: '0',
-          ...(ctaBtn.variant === 'text' && { ...preHeaderNakedButtonStyle }), // To be replaced by 'naked' when implemented on Strapi's side
-        },
-        ...(isValidMuiIcon(ctaBtn.icon) && {
-          startIcon: <Icon>{formatValidMuiIcon(ctaBtn.icon)}</Icon>,
-        }),
-      })),
-    },
-  }),
-  ...(props.rightCtas?.ctaButtons && {
-    rightCtas: {
-      ...props.rightCtas,
-      ctaButtons: props.rightCtas.ctaButtons.map((ctaBtn) => ({
-        ...ctaBtn,
-        target: '_blank',
-        rel: 'noopener noreferrer',
-        disableRipple: ctaBtn.variant === 'text', // To be replaced by 'naked' when implemented on Strapi's side
-        disableTouchRipple: ctaBtn.variant === 'text', // To be replaced by 'naked' when implemented on Strapi's side
-        variant: ctaBtn.variant === 'text' ? 'text' : ctaBtn.variant, // To be replaced by 'naked' when implemented on Strapi's side
-        sx: {
-          fontWeight: '600',
-          letterSpacing: '.3px',
-          ...(ctaBtn.variant === 'text' && { ...preHeaderNakedButtonStyle }), // To be replaced by 'naked' when implemented on Strapi's side
-        },
-        ...(isValidMuiIcon(ctaBtn.icon) && {
-          startIcon: <Icon>{formatValidMuiIcon(ctaBtn.icon)}</Icon>,
-        }),
-      })),
-    },
-  }),
-});
-
-const PreHeader: React.FC<PreHeader['data']['attributes']> = (
-  preHeaderData: PreHeader['data']['attributes']
-) => (
-  // Using sx over styled() because, for styled() to work, the component (in this case PreHeaderEC)
-  // needs to take a className parameter and set it to itself (which PreHeaderEC does not)
-  <Stack
-    sx={{
-      borderBottom: 1,
-      borderBottomColor: 'divider', // Theme-aware property --> equivalent to (theme) => theme.palette.divider
-      minHeight: '48px',
-      padding: '0 24px',
-      display: 'flex',
-      flexDirection: 'row',
-      alignItems: 'center',
-      justifyContent: 'center',
-      '> *': {
-        flex: '1',
-        padding: '0 !important',
-      },
-    }}
-  >
-    {/* Currently casting to PreHeaderProps due to confusing TS error
-    It says that no value other than 'inherit' and 'primary' is valid for a button's color (which is not actually true) */}
-    <PreHeaderEC {...(RefinePreHeaderProps(preHeaderData) as PreHeaderProps)} />
-  </Stack>
+const PreHeader: React.FC<PreHeaderProps> = (preHeaderData) => (
+  <PreHeaderEC {...preHeaderData} />
 );
 
 export default PreHeader;
--- apps/nextjs-website/src/components/rendering/sectionDataToProps.tsx
+++ apps/nextjs-website/src/components/rendering/sectionDataToProps.tsx
@@ -1,27 +1,5 @@
-import { HeroProps } from '@pagopa/pagopa-editorial-components/dist/components/Hero';
 import { EditorialProps } from '@pagopa/pagopa-editorial-components/dist/components/Editorial';
-import { EditorialSection, HeroSection } from '@/lib/fetch/types/PageSection';
-
-export const SectionDataToHeroProps = ({
-  title,
-  subtitle,
-  inverse,
-  size,
-  useHoverlay,
-  image,
-  background,
-  ctaButtons,
-}: HeroSection): HeroProps => ({
-  title,
-  subtitle, // TODO: Parse rich text (markdown)
-  useHoverlay,
-  size,
-  image: image?.url ? 'http://localhost:1337' + image.url : undefined, // TODO: Sub "http://localhost:1337" for Media Library URL
-  altText: image?.alternativeText ?? '',
-  inverse,
-  background: background?.url,
-  ctaButtons,
-});
+import { EditorialSection } from '@/lib/fetch/types/PageSection';
 
 export const SectionDataToEditorialProps = ({
   title,
