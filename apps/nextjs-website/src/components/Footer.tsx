87f8157d7756d2eea2cd6a84f47349572dae863a
Simone Salsi <simone.salsi@dgsspa.com>
2023-12-22 16:45:13 +0100

    Styled Footer to replicate current SEND website

--- apps/nextjs-website/src/components/Footer.tsx
+++ apps/nextjs-website/src/components/Footer.tsx
@@ -4,6 +4,7 @@ import {
   FooterProps,
   Footer as FooterEC,
 } from '@pagopa/pagopa-editorial-components/dist/components/Footer';
+import { Stack } from '@mui/material';
 import { MDtoJSX } from './rendering/MDtoJSX';
 
 const RefineFooterProps = (
@@ -16,6 +17,37 @@ const RefineFooterProps = (
 const Footer: React.FC<Omit<FooterProps, 'onLanguageChanged'>> = (
   footerData
 ) => (
-  <FooterEC {...RefineFooterProps(footerData)} onLanguageChanged={() => true} />
+  <Stack
+    sx={{
+      '.MuiTypography-root.MuiTypography-caption p': {
+        // legalInfo
+        margin: 0,
+      },
+      footer: {
+        padding: 0,
+        borderTop: 1,
+        borderTopColor: 'divider',
+        '> :first-of-type': {
+          // links section
+          '> *': {
+            flex: 1,
+            '&.MuiBox-root svg:not(.MuiSvgIcon-root)': {
+              // NextGenEU logo
+              marginTop: 'auto',
+            },
+            '&.MuiBox-root > :nth-last-child(2)': {
+              // div before NextGenEU logo
+              marginBottom: '24px',
+            },
+          },
+        },
+      },
+    }}
+  >
+    <FooterEC
+      {...RefineFooterProps(footerData)}
+      onLanguageChanged={() => true}
+    />
+  </Stack>
 );
 export default Footer;
