01467173806ec0c2149ce628f47416b668002de5
Meolocious <90854445+Meolocious@users.noreply.github.com>
2024-01-03 15:24:19 +0100

    commit review changes fix

--- apps/nextjs-website/src/components/Hero.tsx
+++ apps/nextjs-website/src/components/Hero.tsx
@@ -2,6 +2,28 @@
 import React from 'react';
 import { Hero as HeroEC } from '@pagopa/pagopa-editorial-components';
 import { HeroProps } from '@pagopa/pagopa-editorial-components/dist/components/Hero/index';
+import { HeroSection } from '@/lib/fetch/types/PageSection';
+
+export const SectionDataToHeroProps = ({
+  title,
+  subtitle,
+  inverse,
+  size,
+  useHoverlay,
+  image,
+  background,
+  ctaButtons,
+}: HeroSection): HeroProps => ({
+  title,
+  subtitle, // TODO: Parse rich text (markdown)
+  useHoverlay,
+  size,
+  image: image?.url ? 'http://localhost:1337' + image.url : undefined, // TODO: Sub "http://localhost:1337" for config.STRAPI_API_BASE_URL
+  altText: image?.alternativeText ?? '',
+  inverse,
+  background: background?.url,
+  ctaButtons,
+});
 
 const Hero: React.FC<HeroProps & { sectionID: string | undefined }> = (
   HeroData
--- apps/nextjs-website/src/components/pageSection/pageSection.tsx
+++ apps/nextjs-website/src/components/pageSection/pageSection.tsx
@@ -1,28 +1,6 @@
 import React from 'react';
-import { HeroProps } from '@pagopa/pagopa-editorial-components/dist/components/Hero';
-import Hero from '../Hero';
-import { HeroSection, PageSection } from '@/lib/fetch/types/PageSection';
-
-const SectionDataToHeroProps = ({
-  title,
-  subtitle,
-  inverse,
-  size,
-  useHoverlay,
-  image,
-  background,
-  ctaButtons,
-}: HeroSection): HeroProps => ({
-  title,
-  subtitle, // TODO: Parse rich text (markdown)
-  useHoverlay,
-  size,
-  image: image?.url ? 'http://localhost:1337' + image.url : undefined, // TODO: Sub "http://localhost:1337" for config.STRAPI_API_BASE_URL
-  altText: image?.alternativeText ?? '',
-  inverse,
-  background: background?.url,
-  ctaButtons,
-});
+import Hero, { SectionDataToHeroProps } from '../Hero';
+import { PageSection } from '@/lib/fetch/types/PageSection';
 
 export function rendering(componentData: PageSection) {
   // Temporarily disable no-small-switch rule (new cases will be added with PRs for other components)
