3c906f8e26e841a2a81cefdd056a82e5a7363149
Meolocious <90854445+Meolocious@users.noreply.github.com>
2024-01-03 16:56:43 +0100

    commit rename page sensitive start

--- apps/nextjs-website/src/components/pageSectiona/pageSectiona.tsx
+++ apps/nextjs-website/src/components/pageSectiona/pageSectiona.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Hero, { SectionDataToHeroProps } from '../Hero';
 import { PageSection } from '@/lib/fetch/types/PageSection';
 
-export function rendering(componentData: PageSection) {
+export default function rendering(componentData: PageSection) {
   // Temporarily disable no-small-switch rule (new cases will be added with PRs for other components)
   // eslint-disable-next-line no-underscore-dangle,sonarjs/no-small-switch
   switch (componentData.__component) {
