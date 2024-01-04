bbeafafe373f3903c9de00b550c4f5e5e9febc75
maurodandrea <98483756+maurodandrea@users.noreply.github.com>
2024-01-04 09:47:39 +0100

    [B2BP-115] - Workflow Deploy CMS (#33)
    * First-Create-Workflow-and-Dockerfile
    
    * Add-docker-file-Prod-Dev
    
    Add-docker-file-Prod-Dev
    
    * modify-path-code_review_infra
    
    add path start workflow
    
    * add-path-strapi-cms-for-run-workflow
    
    * modify-automatic-run-condition
    
    modify-automatic-run-condition
    
    * modify-rule-automatic-start
    
    * Update .github/workflows/deploy_cms.yaml
    
    Co-authored-by: Marco Comi <9998393+kin0992@users.noreply.github.com>
    
    * delete-docker-file-dev
    
    delete-docker-file-dev
    
    * add-docker-file-prod
    
    add-docker-file-prod
    
    * modify-for-review
    
    modify-for-review
    
    * modify-copy-package-json
    
    modify-copy-package-json
    
    * Apply suggestions from code review
    
    Co-authored-by: Marco Comi <9998393+kin0992@users.noreply.github.com>
    
    * insert-var-nodeimage-with-sha
    
    insert-var-nodeimage
    
    * remove blank line
    
    Co-authored-by: Marco Comi <9998393+kin0992@users.noreply.github.com>
    
    * remove-blank-line
    
    Co-authored-by: Marco Comi <9998393+kin0992@users.noreply.github.com>
    
    * modify_name_role_github
    
    * update-tag-image-with-sha
    
    update-tag-image-with-sha
    
    * rename-step-build-final
    
    * update-for-dependency-tsconfig-root-repo
    
    update for resolution of dependency by tsconfig root repo
    
    * output-dns-cms-load-balancer
    
    output-dns-cms-load-balancer
    
    * add-code-for-update-image-tag-and-start-new-ECS-image
    
    add-code-for-update-image-tag-and-start-new-ECS-image
    
    * solution-with-tsconfig-duplicate-and-dockerfile-in-folder-strapi
    
    solution-with-tsconfig-duplicate-and-dockerfile-in-folder-strapi
    
    * update-path-task-def
    
    update-path-task-def
    
    * Fix indentation
    
    * update-release-v510-build-push-action
    
    Co-authored-by: Marco Comi <9998393+kin0992@users.noreply.github.com>
    
    * udate-tags-with-env-var
    
    udate-tags-with-env-var
    
    ---------
    
    Co-authored-by: Marco Comi <9998393+kin0992@users.noreply.github.com>

--- .infrastructure/99_outputs.tf
+++ .infrastructure/99_outputs.tf
@@ -5,3 +5,7 @@ output "terraform_backend_bucket_name" {
 output "terraform_lock_dynamodb_table" {
   value = module.identity.terraform_lock_dynamodb_table
 }
+
+output "cms_dns_load_balancer" {
+  value = aws_alb.cms_load_balancer.dns_name
+}
