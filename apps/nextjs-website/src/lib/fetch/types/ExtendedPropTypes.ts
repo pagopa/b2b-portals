15f2113eea80c5d05571556e7ecfba402569ba71
Simone Salsi <59611877+simosalsi@users.noreply.github.com>
2024-01-09 11:50:20 +0100

    Delete apps/nextjs-website/src/lib/fetch/types/ExtendedPropTypes.ts

--- apps/nextjs-website/src/lib/fetch/types/ExtendedPropTypes.ts
+++
@@ -1,5 +0,0 @@
-import { EditorialProps } from '@pagopa/pagopa-editorial-components/dist/components/Editorial';
-
-export type ExtendedEditorialProps = EditorialProps & {
-  readonly theme: 'light' | 'dark';
-};
