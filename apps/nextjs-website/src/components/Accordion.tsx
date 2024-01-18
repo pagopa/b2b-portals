5c20fd82037023af9c6a88aa9826f8213d2ce72c
Meolocious <90854445+Meolocious@users.noreply.github.com>
2024-01-18 11:51:01 +0100

    commit accordion props integration

--- apps/nextjs-website/src/components/Accordion.tsx
+++ apps/nextjs-website/src/components/Accordion.tsx
@@ -2,9 +2,30 @@
 import React from 'react';
 import { Accordion as AccordionEC } from '@pagopa/pagopa-editorial-components';
 import { AccordionProps } from '@pagopa/pagopa-editorial-components/dist/components/Accordion/Accordion';
+import MarkdownRenderer from './MarkdownRenderer';
+import { AccordionSection } from '@/lib/fetch/types/PageSection';
 
-const Accordion: React.FC<AccordionProps> = (accordionData) => (
-  <AccordionEC {...accordionData} />
+const makeAccordionProps = ({
+  title,
+  subtitle,
+  description,
+  accordionItems,
+  ...rest
+}: AccordionSection): AccordionProps => ({
+  title,
+  subtitle,
+  description: MarkdownRenderer({ markdown: description, variant: 'body2' }),
+  accordionItems: accordionItems.map(({ header, content }) => ({
+    header,
+    content: MarkdownRenderer({ markdown: content, variant: 'body2' }),
+  })),
+  ...rest,
+});
+
+const Accordion = (props: AccordionSection) => (
+  <section>
+    <AccordionEC {...makeAccordionProps(props)} />
+  </section>
 );
 
 export default Accordion;
