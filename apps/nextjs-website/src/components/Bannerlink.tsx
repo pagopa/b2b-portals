fabd033f14e3775bad379691bf1c5a6c7c7c0de7
Simone Salsi <simone.salsi@dgsspa.com>
2024-01-25 11:37:24 +0100

    Updated tests. Made ctaButtons optional. Removed rendering of body.

--- apps/nextjs-website/src/components/Bannerlink.tsx
+++ apps/nextjs-website/src/components/Bannerlink.tsx
@@ -2,30 +2,29 @@
 import { BannerLink as BannerLinkEC } from '@pagopa/pagopa-editorial-components';
 import { BannerLinkProps } from '@pagopa/pagopa-editorial-components/dist/components/BannerLink';
 import { Icon } from '@mui/material';
-import MarkdownRenderer from './MarkdownRenderer';
 import { BannerLinkSection } from '@/lib/fetch/types/PageSection';
 import { formatValidMuiIcon, isValidMuiIcon } from '@/components/Icons';
 
 const makeBannerLinkProps = ({
-  body,
   decoration,
   ctaButtons,
   ...rest
 }: BannerLinkSection): BannerLinkProps => ({
-  body: MarkdownRenderer({ markdown: body, variant: 'body2' }),
   ...(decoration && {
     decoration: {
       url: decoration.url,
       alt: decoration.alternativeText,
     },
   }),
-  ctaButtons: ctaButtons.map((ctaBtn) => ({
-    ...ctaBtn,
-    color: rest.theme === 'dark' ? 'negative' : 'primary',
-    ...(isValidMuiIcon(ctaBtn.icon) && {
-      startIcon: <Icon>{formatValidMuiIcon(ctaBtn.icon)}</Icon>,
-    }),
-  })),
+  ...(ctaButtons.length > 0 && {
+    ctaButtons: ctaButtons.map((ctaBtn) => ({
+      ...ctaBtn,
+      color: rest.theme === 'dark' ? 'negative' : 'primary',
+      ...(isValidMuiIcon(ctaBtn.icon) && {
+        startIcon: <Icon>{formatValidMuiIcon(ctaBtn.icon)}</Icon>,
+      }),
+    })),
+  }),
   ...rest,
 });
 
