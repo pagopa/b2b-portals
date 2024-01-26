ba5be1e747932d0f97f1a86daf9c97d79030db4c
maurodandrea <98483756+maurodandrea@users.noreply.github.com>
2024-01-26 09:48:34 +0100

    update-source-dns-zone
    update-source-dns-zone

--- .infrastructure/11_dns.tf
+++ .infrastructure/11_dns.tf
@@ -1,5 +1,4 @@
 module "dns_zone" {
-  source  = "terraform-aws-modules/route53/aws//modules/zones"
-  version = "~> 2.0"
+  source = "git::github.com/terraform-aws-modules/terraform-aws-route53.git?ref=fb53f9541723161ff39374add047dd8e60441e10" # v2.11.0
   zones   = var.dns_domain_name
 }
