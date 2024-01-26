afd44d58c3488fe805d6cdeb87bff228403bb047
maurodandrea <98483756+maurodandrea@users.noreply.github.com>
2024-01-26 14:33:02 +0100

    fmt

--- .infrastructure/11_dns.tf
+++ .infrastructure/11_dns.tf
@@ -1,5 +1,5 @@
 module "dns_zone" {
-  source  = "git::github.com/terraform-aws-modules/terraform-aws-route53.git?ref=fb53f9541723161ff39374add047dd8e60441e10" # v2.11.0
+  source = "git::github.com/terraform-aws-modules/terraform-aws-route53.git?ref=fb53f9541723161ff39374add047dd8e60441e10" # v2.11.0
 
   zones = {
     "b2bportals.pagopa.it" = {
