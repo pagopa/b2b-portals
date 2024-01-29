fbf3b2edd2562fab9f34f61c276389b8f926845a
maurodandrea <98483756+maurodandrea@users.noreply.github.com>
2024-01-29 18:42:28 +0100

    terraform fmt

--- .infrastructure/12_certs.tf
+++ .infrastructure/12_certs.tf
@@ -1,6 +1,6 @@
 resource "aws_acm_certificate" "cms" {
-  domain_name       = keys(var.dns_domain_name)[0]
-  validation_method = "DNS"
+  domain_name               = keys(var.dns_domain_name)[0]
+  validation_method         = "DNS"
   subject_alternative_names = [format("www.%s", keys(var.dns_domain_name)[0])]
 
   lifecycle {
