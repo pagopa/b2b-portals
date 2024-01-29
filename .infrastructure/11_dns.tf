a2930e5d1a638adae038b6483862b28bbf8fa38b
maurodandrea <98483756+maurodandrea@users.noreply.github.com>
2024-01-29 13:32:14 +0100

    fmt

--- .infrastructure/11_dns.tf
+++ .infrastructure/11_dns.tf
@@ -1,4 +1,4 @@
 module "dns_zone" {
-  source  = "git::github.com/terraform-aws-modules/terraform-aws-route53/modules/zones.git?ref=bc63328714550fd903d2574b263833c9ce1c867e" # v2.11.0"
-  zones   = var.dns_domain_name
+  source = "git::github.com/terraform-aws-modules/terraform-aws-route53/modules/zones.git?ref=bc63328714550fd903d2574b263833c9ce1c867e" # v2.11.0"
+  zones  = var.dns_domain_name
 }
