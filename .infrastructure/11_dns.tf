45f07cd12e3b7c5d17e6d9d88a724d3b730a69b9
maurodandrea <98483756+maurodandrea@users.noreply.github.com>
2024-01-29 22:21:21 +0100

    add-record-for-alb

--- .infrastructure/11_dns.tf
+++ .infrastructure/11_dns.tf
@@ -25,3 +25,21 @@ module "records" {
 
   depends_on = [module.dns_zone]
 }
+
+# record ALB
+module "records" {
+  source = "git::https://github.com/terraform-aws-modules/terraform-aws-route53.git//modules/records?ref=bc63328714550fd903d2574b263833c9ce1c867e" # v2.11.0"
+
+  zone_id = module.dns_zone.route53_zone_zone_id[keys(var.dns_domain_name)[0]]
+
+  records = [
+    {
+      name = "cms"
+      type = "CNAME"
+      ttl  = 3600
+      records = [aws_alb.cms_load_balancer.dns_name]
+    }
+  ]
+
+  depends_on = [module.dns_zone]
+}
