64325b65b6832ee97ff9faedac9558be0fa0c329
maurodandrea <98483756+maurodandrea@users.noreply.github.com>
2024-01-29 15:32:43 +0100

    add-subject_alternative_names

--- .infrastructure/12_certs.tf
+++ .infrastructure/12_certs.tf
@@ -1,6 +1,7 @@
 resource "aws_acm_certificate" "cms" {
   domain_name               = keys(var.dns_domain_name)[0]
   validation_method         = "DNS"
+  subject_alternative_names = [format("www.%s", keys(var.dns_domain_name)[0])]
 
   lifecycle {
     create_before_destroy = true
