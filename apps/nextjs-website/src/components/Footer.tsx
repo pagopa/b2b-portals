4e922a18dde8310f34a8aec5a41bbeb44cdcb2e2
Simone Salsi <59611877+simosalsi@users.noreply.github.com>
2024-01-30 14:32:09 +0100

    [B2BP-255] Simplified Footer (#148)
    * Simplified Footer.
    
    * Added changeset
    
    * Update Footer.tsx
    
    * Update .changeset/wicked-grapes-work.md
    
    Co-authored-by: AF <4775679+datalek@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Simone Salsi <simone.salsi@dgsspa.com>
    Co-authored-by: AF <4775679+datalek@users.noreply.github.com>

--- apps/nextjs-website/src/components/Footer.tsx
+++ apps/nextjs-website/src/components/Footer.tsx
@@ -1,25 +1,73 @@
 'use client';
-import React from 'react';
 import {
   FooterProps,
   Footer as FooterEC,
 } from '@pagopa/pagopa-editorial-components/dist/components/Footer';
 import { Stack } from '@mui/material';
 import MarkdownRenderer from './MarkdownRenderer';
+import { FooterData } from '@/lib/fetch/footer';
 
-const makeFooterProps = (
-  props: Omit<FooterProps, 'onLanguageChanged'>
-): Omit<FooterProps, 'onLanguageChanged'> => ({
-  ...props,
-  legalInfo:
-    typeof props.legalInfo === 'string'
-      ? MarkdownRenderer({ markdown: props.legalInfo, variant: 'caption' })
-      : props.legalInfo,
+const makeFooterProps = ({
+  legalInfo,
+  links_aboutUs,
+  links_services,
+  links_resources,
+  links_followUs,
+  ...rest
+}: FooterData['data']['attributes']): FooterProps => ({
+  legalInfo: MarkdownRenderer({
+    markdown: legalInfo,
+    variant: 'caption',
+  }),
+  links: {
+    aboutUs: {
+      links: links_aboutUs.links.map((link) => ({
+        linkType: 'internal', // unused
+        ...link,
+      })),
+      ...(links_aboutUs.title && { title: links_aboutUs.title }),
+    },
+    services: {
+      links: links_services.links.map((link) => ({
+        linkType: 'internal', // unused
+        ...link,
+      })),
+      ...(links_services.title && { title: links_services.title }),
+    },
+    resources: {
+      links: links_resources.links.map((link) => ({
+        linkType: 'internal', // unused
+        ...link,
+      })),
+      ...(links_resources.title && { title: links_resources.title }),
+    },
+    followUs: {
+      title: links_followUs.title,
+      links: links_followUs.links.map((link) => ({
+        linkType: 'internal', // unused
+        ...link,
+      })),
+      socialLinks: links_followUs.socialLinks.map((link) => ({
+        linkType: 'internal', // unused
+        ...link,
+      })),
+    },
+  },
+  languages: [
+    {
+      id: 'it',
+      value: 'Italiano',
+    },
+  ],
+  activeLanguage: {
+    id: 'it',
+    value: 'Italiano',
+  },
+  onLanguageChanged: () => true,
+  ...rest,
 });
 
-const Footer: React.FC<Omit<FooterProps, 'onLanguageChanged'>> = (
-  footerData
-) => (
+const Footer = (props: FooterData['data']['attributes']) => (
   <Stack
     sx={{
       '.MuiTypography-root.MuiTypography-caption p': {
@@ -47,7 +95,7 @@ const Footer: React.FC<Omit<FooterProps, 'onLanguageChanged'>> = (
       },
     }}
   >
-    <FooterEC {...makeFooterProps(footerData)} onLanguageChanged={() => true} />
+    <FooterEC {...makeFooterProps(props)} />
   </Stack>
 );
 export default Footer;
