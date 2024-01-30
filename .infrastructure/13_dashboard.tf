e910e424f89a871cf7e244c47bedc68fe17d6c1d
maurodandrea <98483756+maurodandrea@users.noreply.github.com>
2024-01-30 17:51:34 +0100

    first-dash

---
+++ .infrastructure/13_dashboard.tf
@@ -0,0 +1,14 @@
+resource "aws_cloudwatch_dashboard" "main" {
+  dashboard_name = "Main"
+
+  dashboard_body = templatefile("${path.module}/dashboards/main.json",
+    {
+      aws_region              = var.aws_region
+      cf_distribution_id      = aws_cloudfront_distribution.website.id,
+      ecs_cluster_name        = aws_ecs_cluster.cms_ecs_cluster.name
+      ecs_cms_service_name    = aws_ecs_service.cms_ecs_service.name,
+      rds_aurora_cluster_name = aws_rds_cluster.cms_database_cluster.cluster_identifier
+      alb_fe_arn_suffix       = "",
+    }
+  )
+}
