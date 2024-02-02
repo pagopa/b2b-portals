fe00e0fc27963f54125051acc6a036434ff84d52
Simone Salsi <simone.salsi@dgsspa.com>
2024-02-02 16:32:50 +0100

    Updated makeEditorialProps

--- apps/nextjs-website/src/components/Editorial.tsx
+++ apps/nextjs-website/src/components/Editorial.tsx
@@ -17,13 +17,16 @@ const makeEditorialProps = ({
   ...(eyelet && { eyelet }),
   body: MarkdownRenderer({ markdown: body, variant: 'body2' }),
   image: <img src={image.url} alt={image.alternativeText ?? undefined} />,
-  ctaButtons: ctaButtons.map(({ icon, ...ctaBtn }) => ({
-    ...ctaBtn,
-    color: theme === 'dark' ? 'negative' : 'primary',
-    ...(icon && {
-      startIcon: <Icon>{formatValidMuiIcon(icon)}</Icon>,
+  ...(ctaButtons &&
+    ctaButtons.length > 0 && {
+      ctaButtons: ctaButtons.map(({ icon, ...ctaBtn }) => ({
+        ...ctaBtn,
+        color: theme === 'dark' ? 'negative' : 'primary',
+        ...(icon && {
+          startIcon: <Icon>{formatValidMuiIcon(icon)}</Icon>,
+        }),
+      })),
     }),
-  })),
   ...rest,
 });
 
