391240d5374c0f735d3349398a59597cf4d0fe7a
Simone Salsi <simone.salsi@dgsspa.com>
2024-02-02 12:46:53 +0100

    Simplify Hero Section

--- apps/nextjs-website/src/lib/fetch/types/PageSection.ts
+++ apps/nextjs-website/src/lib/fetch/types/PageSection.ts
@@ -1,5 +1,5 @@
 import * as t from 'io-ts';
-import { CTAButtonSchema } from './CTAButton';
+import { CTAButtonSchema, CTAButton_SimpleSchema } from './CTAButton';
 import { ImageDataCodec } from './StrapiImage';
 
 const HeroSectionCodec = t.strict({
@@ -12,11 +12,10 @@ const HeroSectionCodec = t.strict({
     small: null,
     big: null,
   }),
-  useHoverlay: t.boolean,
   sectionID: t.union([t.string, t.null]),
   image: t.union([ImageDataCodec, t.null]),
   background: t.union([ImageDataCodec, t.null]),
-  ctaButtons: t.array(CTAButtonSchema),
+  ctaButtons: t.array(CTAButton_SimpleSchema),
 });
 
 const EditorialSectionCodec = t.strict({
