d2738d3e6baff5c7b47da193bc12f7a8766b91bd
maurodandrea <98483756+maurodandrea@users.noreply.github.com>
2024-02-06 21:50:05 +0100

    update

--- .infrastructure/11_dns.tf
+++ .infrastructure/11_dns.tf
@@ -13,7 +13,7 @@ module "records" {
       name    = ""
       type    = "NS"
       ttl     = 3600
-      records = [module.dns_zone.route53_zone_name_servers]
+      records = [keys(module.dns_zone.route53_zone_name_servers)[0]]
     },
     {
       name    = ""
