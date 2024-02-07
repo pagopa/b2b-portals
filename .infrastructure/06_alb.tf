25df6a235826e507c05d05aaac119d2d86adadc2
maurodandrea <98483756+maurodandrea@users.noreply.github.com>
2024-02-07 09:23:20 +0100

    update-targetgroup

--- .infrastructure/06_alb.tf
+++ .infrastructure/06_alb.tf
@@ -49,6 +49,6 @@ resource "aws_lb_listener" "front_end_https" {
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.front_end.arn
+    target_group_arn = aws_alb_target_group.cms.arn
   }
 }
