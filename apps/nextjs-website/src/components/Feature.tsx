e1e14eda3ef48dbe0857348b387fa528c4ae2e6e
Meolocious <90854445+Meolocious@users.noreply.github.com>
2024-02-07 16:17:07 +0100

    [B2BP-251] Styling Feature Component (#158)
    * commit style to feature
    
    * commit link font fix
    
    * commit changeset
    
    * Updated styling taking into account the mobile carousel use case
    
    * Updated icons and buttons color
    
    * Added background using value found in SEND website
    
    * Set items width to 100% to prevent them collapsing when text is short for all items
    
    * Update Feature.tsx
    
    * Prettier
    
    ---------
    
    Co-authored-by: Simone Salsi <simone.salsi@dgsspa.com>
    Co-authored-by: Simone Salsi <59611877+simosalsi@users.noreply.github.com>

--- apps/nextjs-website/src/components/Feature.tsx
+++ apps/nextjs-website/src/components/Feature.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { Feature as FeatureEC } from '@pagopa/pagopa-editorial-components';
 import { FeatureProps } from '@pagopa/pagopa-editorial-components/dist/components/Feature/Feature';
+import { Stack, useTheme } from '@mui/material';
 import { isValidMuiIcon } from './Icons';
 import { FeatureSection } from '@/lib/fetch/types/PageSection';
 
@@ -25,10 +26,58 @@ const makeFeatureProps = ({
   ...rest,
 });
 
-const Feature = (props: FeatureSection) => (
-  <section id={props.sectionID || undefined}>
-    <FeatureEC {...makeFeatureProps(props)} />
-  </section>
-);
+const Feature = (props: FeatureSection) => {
+  const theme = useTheme();
+
+  return (
+    <section id={props.sectionID || undefined}>
+      <Stack
+        sx={{
+          section: {
+            ...(props.theme === 'light' && {
+              backgroundColor: 'background.default',
+            }),
+          },
+          '.MuiGrid-item': {
+            width: '100%',
+          },
+          '.MuiLink-root': {
+            // Buttons
+            fontFamily: '"Titillium Web",sans-serif;',
+            color:
+              props.theme === 'dark' ? 'primary.contrastText' : 'primary.main',
+          },
+          '.MuiSvgIcon-root': {
+            // Buttons Arrow + Icons
+            ...(props.theme === 'light' && { color: 'primary.main' }),
+          },
+          '.MuiTypography-root.MuiTypography-body1': {
+            // Icons container
+            textAlign: 'center',
+          },
+          '.MuiMobileStepper-root': {
+            // Carousel dots container
+            backgroundColor: 'transparent',
+          },
+          [theme.breakpoints.down('md')]: {
+            // Mobile view (vertically stacked items or carousel)
+            '.MuiGrid-item': {
+              // Items
+              width: '100%',
+              '.MuiTypography-body2': {
+                // Subtitle
+                maxWidth: 300,
+                marginLeft: 'auto',
+                marginRight: 'auto',
+              },
+            },
+          },
+        }}
+      >
+        <FeatureEC {...makeFeatureProps(props)} />
+      </Stack>
+    </section>
+  );
+};
 
 export default Feature;
