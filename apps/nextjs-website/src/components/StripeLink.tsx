c88ee64a41d67c3df0407fe3bf465e4f95b51b26
Meolocious <90854445+Meolocious@users.noreply.github.com>
2024-02-14 10:46:58 +0100

    commit StripeLink component

--- apps/nextjs-website/src/components/StripeLink.tsx
+++ apps/nextjs-website/src/components/StripeLink.tsx
@@ -1,28 +1,23 @@
 'use client';
-import React from 'react';
-import { EIconProps } from '@pagopa/pagopa-editorial-components/dist/components/EIcon';
-import { ReactElement } from 'react';
+import { StripeLink as StripeLinkEC } from '@pagopa/pagopa-editorial-components';
+import { StripeLinkProps } from '@pagopa/pagopa-editorial-components/dist/components/StripeLink';
 import { Icon } from '@mui/material';
-import { formatValidMuiIcon, isValidMuiIcon } from './Icons';
-import MarkdownRenderer from './MarkdownRenderer';
 import { StripeLinkSection } from '@/lib/fetch/types/PageSection';
-
-interface StripeLinkProps {
-  theme: 'dark' | 'light';
-  subtitle: string | ReactElement;
-  icon?: EIconProps;
-  buttonText: string;
-}
+import { formatValidMuiIcon } from '@/components/Icons';
 
 const makeStripeLinkProps = ({
+  theme,
   subtitle,
   icon,
+  buttonText,
   ...rest
 }: StripeLinkSection): StripeLinkProps => ({
-  subtitle: MarkdownRenderer({ markdown: subtitle, variant: 'body2' }),
-  ...(isValidMuiIcon(icon) && {
-    icon: <Icon>{formatValidMuiIcon(icon)}</Icon>,
+  theme,
+  subtitle,
+  ...(icon && {
+    startIcon: <Icon>{formatValidMuiIcon(icon)}</Icon>,
   }),
+  buttonText,
   ...rest,
 });
 
@@ -33,30 +28,3 @@ const StripeLink = (props: StripeLinkSection) => (
 );
 
 export default StripeLink;
-
-// Versione SEND legacy
-
-// import { Box, Button, Container, Grid, Typography } from '@mui/material';
-// import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
-// import CodeIcon from '@mui/icons-material/Code';
-// export const StripeLink = () => {
-//     return (
-//         <Box sx={{ backgroundColor: 'info.contrastText' }} padding={2}>
-//             <Container maxWidth='xl'>
-//                 <Grid container spacing={2} >
-//                     <Grid item xs={12} md={6} display={'flex'} justifyContent={'center'} alignItems={'center'}>
-//                         <Typography variant='body2' color='white' display={'flex'} justifyContent={'center'} alignItems={'center'}>
-//                             <CodeIcon sx={{ fontSize: '20px', marginRight: '10px' }} />
-//                             Il tuo ente ha già aderito e hai bisogno di approfondire le fasi di integrazione alla Piattaforma?
-//                         </Typography>
-//                     </Grid>
-//                     <Grid item xs={12} md={6}>
-//                         <Button variant='contained' color='primary' endIcon={<ArrowForwardIcon />} href='https://developer.pagopa.it/send/overview'>
-//                             Vai alla documentazione tecnica
-//                         </Button>
-//                     </Grid>
-//                 </Grid>
-//             </Container>
-//         </Box>
-//     );
-// };
