1a911240d3f67e4be5e96f3a06af7bc9f280e7b9
Simone Salsi <simone.salsi@dgsspa.com>
2024-02-22 17:30:09 +0100

    Reduce AllowedMUIIcon to only currently-used icons

--- apps/nextjs-website/src/components/MUIIcon.tsx
+++ apps/nextjs-website/src/components/MUIIcon.tsx
@@ -1,26 +1,4 @@
-import {
-  AccountBalanceWalletOutlined,
-  AutoFixHighOutlined,
-  CachedRounded,
-  CheckCircleOutlined,
-  CloudOutlined,
-  EnergySavingsLeafOutlined,
-  Facebook,
-  ForwardToInboxOutlined,
-  GradingOutlined,
-  GroupAddOutlined,
-  HelpOutlineOutlined,
-  HourglassEmptyRounded,
-  HubOutlined,
-  Instagram,
-  LinkedIn,
-  MarkEmailReadOutlined,
-  MarkunreadMailboxOutlined,
-  NotificationsActiveOutlined,
-  SavingsOutlined,
-  Twitter,
-  UploadFileOutlined,
-} from '@mui/icons-material';
+import { HelpOutlineOutlined } from '@mui/icons-material';
 import { AllowedMUIIcon } from '@/lib/fetch/types/mui/AllowedIcon';
 
 // Return requested MUI Icon
@@ -30,32 +8,6 @@ import { AllowedMUIIcon } from '@/lib/fetch/types/mui/AllowedIcon';
 const iconMapping = {
   // Button Icons
   HelpOutlineOutlined: <HelpOutlineOutlined />,
-
-  // Feature Item Icons
-  HubOutlined: <HubOutlined />,
-  AutoFixHighOutlined: <AutoFixHighOutlined />,
-  MarkunreadMailboxOutlined: <MarkunreadMailboxOutlined />,
-  CheckCircleOutlined: <CheckCircleOutlined />,
-  SavingsOutlined: <SavingsOutlined />,
-  HourglassEmptyRounded: <HourglassEmptyRounded />,
-  EnergySavingsLeafOutlined: <EnergySavingsLeafOutlined />,
-  CloudOutlined: <CloudOutlined />,
-
-  // HowTo Step Icons
-  UploadFileOutlined: <UploadFileOutlined />,
-  CachedRounded: <CachedRounded />,
-  ForwardToInboxOutlined: <ForwardToInboxOutlined />,
-  MarkEmailReadOutlined: <MarkEmailReadOutlined />,
-  NotificationsActiveOutlined: <NotificationsActiveOutlined />,
-  GradingOutlined: <GradingOutlined />,
-  AccountBalanceWalletOutlined: <AccountBalanceWalletOutlined />,
-  GroupAddOutlined: <GroupAddOutlined />,
-
-  // Social Icons
-  Instagram: <Instagram />,
-  LinkedIn: <LinkedIn />,
-  Facebook: <Facebook />,
-  Twitter: <Twitter />,
 };
 
 const MUIIcon = (name: AllowedMUIIcon): JSX.Element => iconMapping[name];
--- apps/nextjs-website/src/lib/fetch/types/mui/AllowedIcon.ts
+++ apps/nextjs-website/src/lib/fetch/types/mui/AllowedIcon.ts
@@ -1,13 +1,5 @@
 import * as t from 'io-ts';
 import { MUIButtonIconCodec } from './ButtonIcon';
-import { FeatureItemMUIIconCodec } from './FeatureItemIcon';
-import { HowToStepMUIIconCodec } from './HowToStepIcon';
-import { MUISocialIconCodec } from './SocialIcon';
 
-const AllowedMUIIconCodec = t.union([
-  MUIButtonIconCodec,
-  FeatureItemMUIIconCodec,
-  HowToStepMUIIconCodec,
-  MUISocialIconCodec,
-]);
+const AllowedMUIIconCodec = MUIButtonIconCodec;
 export type AllowedMUIIcon = t.TypeOf<typeof AllowedMUIIconCodec>;
