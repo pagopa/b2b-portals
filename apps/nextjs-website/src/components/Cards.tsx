a4938d5744224c366afa2a885c144c45bd8c48de
Meolocious <90854445+Meolocious@users.noreply.github.com>
2024-02-28 09:45:24 +0100

    commit cards style

--- apps/nextjs-website/src/components/Cards.tsx
+++ apps/nextjs-website/src/components/Cards.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { Cards as CardsEC } from '@pagopa/pagopa-editorial-components';
 import { CardsProps } from '@pagopa/pagopa-editorial-components/dist/components/Cards';
+import { Stack } from '@mui/material';
 import MarkdownRenderer from './MarkdownRenderer';
 import { CardsSection } from '@/lib/fetch/types/PageSection';
 
@@ -43,7 +44,42 @@ const makeCardsProps = ({
 
 const Cards = (props: CardsSection) => (
   <section id={props.sectionID || undefined}>
-    <CardsEC {...makeCardsProps(props)} />
+    <Stack
+      sx={{
+        section: {
+          ...(props.theme === 'dark' && { backgroundColor: 'pagoPA.main' }),
+          img: {
+            display: 'none',
+          },
+        },
+        '.MuiSvgIcon-fontSizeMedium': {
+          width: '2rem',
+          height: '2rem',
+          marginLeft: '0.5rem',
+        },
+        '.MuiLink-root': {
+          fontFamily: '"Titillium Web",sans-serif;',
+          fontSize: '1.125rem', // 18px
+          fontWeight: 700,
+        },
+        '.MuiTypography-body2': {
+          color:
+            props.theme === 'dark' ? 'primary.contrastText' : 'primary.main',
+        },
+        '.MuiTypography-body1': {
+          fontSize: '1.125rem', // 18px
+        },
+        '.MuiTypography-h2': {
+          fontSize: '2rem', // 32px
+          fontWeight: 700,
+        },
+        '.MuiTypography-h6': {
+          fontWeight: 700,
+        },
+      }}
+    >
+      <CardsEC {...makeCardsProps(props)} />
+    </Stack>
   </section>
 );
 
