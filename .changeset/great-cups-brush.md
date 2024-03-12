96f9c2eac21d629e7f85f899615f3929af114410
Simone Salsi <simone.salsi@dgsspa.com>
2024-03-12 15:15:51 +0100

    Create DB schema, if it doesn't exist, before running server

---
+++ .changeset/great-cups-brush.md
@@ -0,0 +1,5 @@
+---
+"strapi-cms": minor
+---
+
+Create DB schema, if it doesn't exist, before running server
--- apps/strapi-cms/src/index.ts
+++ apps/strapi-cms/src/index.ts
@@ -1,1 +1,22 @@
-export default {};
+import { Client } from 'pg';
+/* eslint-disable functional/no-expression-statements */
+
+export default {
+  register: async (): Promise<void> => {
+    const client = new Client({
+      application_name: 'strapi',
+      database: process.env['DATABASE_NAME'],
+      host: process.env['DATABASE_HOST'],
+      password: process.env['DATABASE_PASSWORD'],
+      port: Number(process.env['DATABASE_PORT']),
+      query_timeout: 5000,
+      statement_timeout: 5000,
+      user: process.env['DATABASE_USERNAME'],
+    });
+    await client.connect();
+    await client.query(
+      `CREATE SCHEMA IF NOT EXISTS ${process.env['DATABASE_SCHEMA']};`
+    );
+    await client.end();
+  },
+};
