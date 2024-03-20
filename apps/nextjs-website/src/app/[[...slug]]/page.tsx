d407211c1d10ac46b388139f607d4134d8cd2dc1
Simone Salsi <simone.salsi@dgsspa.com>
2024-03-20 13:06:46 +0100

    Update NextJS to 14.1.0

--- apps/nextjs-website/src/app/[[...slug]]/page.tsx
+++ apps/nextjs-website/src/app/[[...slug]]/page.tsx
@@ -3,10 +3,6 @@ import { Page } from '@/lib/pages';
 import { getAllPages, getPageProps, getSiteWideSEO } from '@/lib/api';
 import PageSection from '@/components/PageSection/PageSection';
 
-// Dynamic segments not included in generateStaticParams will return a 404.
-// more: https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config#dynamicparams
-export const dynamicParams = false;
-
 // Statically generate routes at build time instead of on-demand at request time.
 // more: https://nextjs.org/docs/app/building-your-application/routing/dynamic-routes#generating-static-params
 export const generateStaticParams = async (): Promise<Page[]> =>
