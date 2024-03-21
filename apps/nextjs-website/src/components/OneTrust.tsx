72235b3e254997222150c4770c513ee065bce7f7
Simone Salsi <simone.salsi@dgsspa.com>
2024-03-21 10:50:37 +0100

    Style OneTrustSection

--- apps/nextjs-website/src/components/OneTrust.tsx
+++ apps/nextjs-website/src/components/OneTrust.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { useEffect } from 'react';
+import { Box } from '@mui/material';
 import { ExtractNoticeIDFromOneTrustURL } from '@/lib/oneTrust';
 import { OneTrustSectionProps } from '@/lib/fetch/types/PageSection';
 
@@ -34,7 +35,65 @@ const OneTrustSection = (props: OneTrustSectionProps) => {
   // After fetching the given notice,
   // otnotice-1.0.min.js will look for the below div to populate.
   // To do so it will search for the specific divID
-  return <div id={divID} />;
+  return (
+    <Box
+      id={divID}
+      sx={{
+        '.otnotice-content': {
+          fontFamily: '"Titillium Web",sans-serif',
+          // eslint-disable-next-line sonarjs/no-duplicate-string
+          color: 'text.primary',
+          paddingTop: '64px',
+        },
+        '.otnotice-content .otnotice-menu': {
+          position: 'absolute',
+          backgroundColor: 'transparent',
+          border: 'none',
+          boxShadow: 'none',
+          maxHeight: 'none',
+          marginLeft: 0,
+          paddingTop: 0,
+        },
+        '.otnotice-content .otnotice-menu-mobile': {
+          backgroundColor: 'transparent',
+          border: 'none',
+          boxShadow: 'custom.otMenuMobile',
+        },
+        '.otnotice-content .otnotice-menu-mobile .otnotice-menu-mobile-container':
+          {
+            backgroundColor: 'transparent',
+          },
+        '.otnotice-content .otnotice-menu > .otnotice-menu-section': {
+          margin: 0,
+          padding: '1rem',
+          width: '100%',
+        },
+        '.otnotice-content .otnotice-menu > .otnotice-menu-section:hover, .otnotice-content .otnotice-menu-mobile .otnotice-menu-section-mobile:hover':
+          {
+            backgroundColor: 'action.hover',
+          },
+        '.otnotice-content .otnotice-menu > .otnotice-menu-section a, .otnotice-content .otnotice-menu-mobile .otnotice-menu-selected, .otnotice-content .otnotice-menu-mobile .otnotice-menu-section-mobile a':
+          {
+            fontSize: '1.125rem',
+            fontWeight: 600,
+            lineHeight: 1.5,
+            color: 'text.primary !important',
+          },
+        '.otnotice-menu-mobile .otnotice-menu-selected-container .otnotice-menu-display__expand::after, .otnotice-menu-mobile .otnotice-menu-selected-container .otnotice-menu-display__collapse::after':
+          {
+            color: 'text.primary !important',
+          },
+        '.otnotice-content .otnotice-section:first-child h2': {
+          fontSize: '2.375rem',
+          color: 'text.primary',
+        },
+        '.otnotice-content .otnotice-section:not(:first-child) h2': {
+          fontSize: '1.75rem',
+          color: 'text.primary',
+        },
+      }}
+    />
+  );
 };
 
 export default OneTrustSection;
