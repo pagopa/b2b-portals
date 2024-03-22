6200adf26e7e037adcf0c4ebfb989df6f5c8adf9
Meolocious <90854445+Meolocious@users.noreply.github.com>
2024-03-22 09:17:49 +0100

    commit lint fix

--- apps/nextjs-website/src/components/BannerLink.tsx
+++ apps/nextjs-website/src/components/BannerLink.tsx
@@ -1,10 +1,10 @@
-"use client";
-import { BannerLink as BannerLinkEC } from "@pagopa/pagopa-editorial-components";
-import { BannerLinkProps } from "@pagopa/pagopa-editorial-components/dist/components/BannerLink";
-import MarkdownRenderer from "./MarkdownRenderer";
-import { BannerLinkSection } from "@/lib/fetch/types/PageSection";
-import Icon from "@/components/Icon";
-import { Stack } from "@mui/material";
+'use client';
+import { BannerLink as BannerLinkEC } from '@pagopa/pagopa-editorial-components';
+import { BannerLinkProps } from '@pagopa/pagopa-editorial-components/dist/components/BannerLink';
+import { Stack } from '@mui/material';
+import MarkdownRenderer from './MarkdownRenderer';
+import { BannerLinkSection } from '@/lib/fetch/types/PageSection';
+import Icon from '@/components/Icon';
 
 const makeBannerLinkProps = ({
   body,
@@ -12,19 +12,19 @@ const makeBannerLinkProps = ({
   ctaButtons,
   ...rest
 }: BannerLinkSection): BannerLinkProps => ({
-  body: MarkdownRenderer({ markdown: body, variant: "body2" }),
+  body: MarkdownRenderer({ markdown: body, variant: 'body2' }),
   ...(decoration && {
     decoration: {
       src: decoration.url,
       alt: decoration.alternativeText,
-      width: "60px",
-      height: "60px",
+      width: '60px',
+      height: '60px',
     },
   }),
   ...(ctaButtons.length > 0 && {
     ctaButtons: ctaButtons.map(({ icon, ...ctaBtn }) => ({
       ...ctaBtn,
-      color: rest.theme === "dark" ? "negative" : "primary",
+      color: rest.theme === 'dark' ? 'negative' : 'primary',
       ...(icon && { startIcon: Icon(icon) }),
     })),
   }),
@@ -35,9 +35,9 @@ const BannerLink = (props: BannerLinkSection) => (
   <section id={props.sectionID || undefined}>
     <Stack
       sx={{
-        ".MuiTypography-body2": {
+        '.MuiTypography-body2': {
           color:
-            props.theme === "dark" ? "primary.contrastText" : "text.primary",
+            props.theme === 'dark' ? 'primary.contrastText' : 'text.primary',
         },
       }}
     >
