e1f99f4a2d459542e5bac56c6d5d1b78a3058299
Simone Salsi <simone.salsi@dgsspa.com>
2024-03-28 11:53:01 +0100

    Made ExtractNoticeIDFromOneTrustURL safer

--- apps/nextjs-website/src/lib/__tests__/oneTrust.test.ts
+++ apps/nextjs-website/src/lib/__tests__/oneTrust.test.ts
@@ -10,13 +10,23 @@ describe('ExtractNoticeIDFromOneTrustURL', () => {
   });
   it('should return null from a non-OneTrust URL', () => {
     const actual = ExtractNoticeIDFromOneTrustURL(
-      'https://non-onetrust-url-example.othersite.com/client-id/service-id/draft/noticeID.json'
+      'https://non-onetrust.com-url-example.othersite.com/fake.onetrust.com/service-id/draft/noticeID.json'
     );
     expect(actual).toBeNull();
   });
   it('should return null from a URL that does not point to a .json file', () => {
     const actual = ExtractNoticeIDFromOneTrustURL(
       'https://no-json-url-example.onetrust.com/client-id/service-id/draft/noticeID'
+    );
+    expect(actual).toBeNull();
+  });
+  it('should return null from an invalid URL', () => {
+    const actual = ExtractNoticeIDFromOneTrustURL('not-a-url');
+    expect(actual).toBeNull();
+  });
+  it('should only accept https URLs', () => {
+    const actual = ExtractNoticeIDFromOneTrustURL(
+      'http://unsafe-but-valid-url-example.onetrust.com/client-id/service-id/draft/noticeID.json'
     );
     expect(actual).toBeNull();
   });
--- apps/nextjs-website/src/lib/oneTrust.ts
+++ apps/nextjs-website/src/lib/oneTrust.ts
@@ -1,15 +1,21 @@
 export const ExtractNoticeIDFromOneTrustURL = (
-  oneTrustNoticeURL: string
+  OTNoticeURLString: string
 ): string | null => {
-  // Check that given URL comes from onetrust.com and points to a .json file as it should
+  // Check that given URL comes from onetrust.com and points to a .json file as it should before parsing
+  const OTNoticeURL = URL.canParse(OTNoticeURLString)
+    ? new URL(OTNoticeURLString)
+    : null;
+
   if (
-    !oneTrustNoticeURL.includes('onetrust.com/') ||
-    !oneTrustNoticeURL.endsWith('.json')
+    OTNoticeURL === null ||
+    OTNoticeURL.protocol !== 'https:' ||
+    !OTNoticeURL.hostname.endsWith('onetrust.com') ||
+    !OTNoticeURL.pathname.endsWith('.json')
   ) {
     return null;
   }
 
-  const splitURL = oneTrustNoticeURL.split('/');
+  const splitURL = OTNoticeURL.pathname.split('/');
   const fileName = splitURL[splitURL.length - 1];
 
   return fileName?.split('.')[0] ?? null;
