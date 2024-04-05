a382556848a8a7c511636656bcd6d0b589863a59
salvatorediocaro <salvatore.diocaro@gmail.com>
2024-04-05 12:02:23 +0200

    B2BP-427 modified hero layout

--- apps/nextjs-website/src/components/react-components/src/components/Hero/Hero.tsx
+++ apps/nextjs-website/src/components/react-components/src/components/Hero/Hero.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Image from 'next/image';
 import { Box, Stack, Typography, Grid } from '@mui/material';
 import { isJSX } from '../../utils';
 import EContainer from '../EContainer';
