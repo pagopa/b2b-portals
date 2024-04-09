c951863cfabcdc8ed9b2235d62d0a3a7d66dba94
Meolocious <90854445+Meolocious@users.noreply.github.com>
2024-04-09 13:07:58 +0200

    commit editorial inside B2BP

--- apps/nextjs-website/src/components/Editorial.tsx
+++ apps/nextjs-website/src/components/Editorial.tsx
@@ -1,8 +1,7 @@
 'use client';
-import { Editorial as EditorialEC } from '@pagopa/pagopa-editorial-components';
-import { EditorialProps } from '@pagopa/pagopa-editorial-components/dist/components/Editorial';
-import { Stack } from '@mui/material';
 import Image from 'next/image';
+import { Editorial as EditorialEC } from './react-components/src';
+import { EditorialProps } from './react-components/src/index-props';
 import MarkdownRenderer from './MarkdownRenderer';
 import { EditorialSection } from '@/lib/fetch/types/PageSection';
 import Icon from '@/components/Icon';
@@ -16,13 +15,13 @@ const makeEditorialProps = ({
   storeButtons,
   ...rest
 }: EditorialSection): EditorialProps => ({
+  theme,
   ...(eyelet && { eyelet }),
   body: MarkdownRenderer({ markdown: body, variant: 'body2' }),
   image: (
     <Image
-      src={image.url}
+      src={`http://localhost:1337${image.url}`}
       alt={image.alternativeText ?? ''}
-      // Needed by Image, will be overwritten
       width={0}
       height={0}
     />
@@ -44,47 +43,8 @@ const makeEditorialProps = ({
   ...rest,
 });
 
-const Editorial = (props: EditorialSection) => {
-  const themeColor =
-    props.theme === 'dark' ? 'primary.contrastText' : 'text.primary';
-
-  return (
-    <section id={props.sectionID || undefined}>
-      <Stack
-        sx={{
-          section: {
-            ...(props.theme === 'dark' && {
-              backgroundColor: 'custom.backgroundColorDark',
-            }),
-          },
-          img: {
-            maxHeight: 490,
-            width: '100%',
-          },
-          '.MuiTypography-root': {
-            color: themeColor,
-            a: {
-              color: themeColor,
-              fontWeight: 'bold',
-            },
-          },
-          '.MuiTypography-h4': {
-            color: themeColor,
-          },
-          '.MuiTypography-body2': {
-            color: themeColor,
-          },
-          '.MuiTypography-overline': {
-            ...(props.theme === 'light' && {
-              color: 'text.secondary',
-            }),
-          },
-        }}
-      >
-        <EditorialEC {...makeEditorialProps(props)} />
-      </Stack>
-    </section>
-  );
-};
+const Editorial = (props: EditorialSection) => (
+  <EditorialEC {...makeEditorialProps(props)} />
+);
 
 export default Editorial;
