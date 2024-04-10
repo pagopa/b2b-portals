d38e7c72b72dcc727cda84d2065ffb33a5ec99df
Meolocious <90854445+Meolocious@users.noreply.github.com>
2024-04-10 13:16:12 +0200

    commit implementing HowTo inside B2BP

--- apps/nextjs-website/src/components/react-components/src/index-props.ts
+++ apps/nextjs-website/src/components/react-components/src/index-props.ts
@@ -1,4 +1,5 @@
 import { HeroProps } from './utils/Components.types';
 import { EditorialProps } from './utils/Components.types';
+import { HowToProps } from './utils/Components.types';
 
-export type { HeroProps, EditorialProps };
+export type { HeroProps, EditorialProps, HowToProps };
--- apps/nextjs-website/src/components/react-components/src/utils/Components.types.ts
+++ apps/nextjs-website/src/components/react-components/src/utils/Components.types.ts
@@ -1,6 +1,7 @@
 import { ReactElement } from 'react';
 import { type ButtonProps } from '@mui/material';
 import { type CommonProps, type Generic } from '../types/components';
+import { EIconProps } from '../components/EIcon';
 
 {
   /* COMMON PROPS */
@@ -64,4 +65,33 @@ export interface StoreButtonsProps {
 export interface EditorialCtaProps extends CommonProps {
   readonly ctaButtons?: ReadonlyArray<CtaButtonProps>;
   readonly storeButtons?: StoreButtonsProps;
+}
+
+{
+  /* HOWTO PROPS */
+}
+
+export interface Step {
+  readonly stepIcon?: EIconProps;
+  readonly title: string;
+  readonly description: string | JSX.Element;
+}
+
+export interface HowToStepProps extends Step {
+  readonly index: number;
+  readonly theme: 'light' | 'dark';
+  readonly isLastStep: boolean;
+}
+
+export interface HowToProps {
+  readonly title: string;
+  readonly steps: ReadonlyArray<Step>;
+  readonly theme: 'light' | 'dark';
+  readonly link?: {
+    readonly href: string;
+    readonly label: string;
+    readonly target?: React.HTMLAttributeAnchorTarget;
+  };
+  readonly rowMaxSteps?: number;
+  readonly stepsAlignment?: 'right' | 'center' | 'left';
 }
