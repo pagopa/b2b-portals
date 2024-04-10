5ca3df1aa85304b714c7f54b01e0c49b4da9635a
Meolocious <90854445+Meolocious@users.noreply.github.com>
2024-04-10 13:16:12 +0200

    commit implementing HowTo inside B2BP

--- apps/nextjs-website/src/components/react-components/src/index.ts
+++ apps/nextjs-website/src/components/react-components/src/index.ts
@@ -1,4 +1,5 @@
 import Hero from './components/Hero/Hero';
 import Editorial from './components/Editorial/Editorial';
+import HowTo from './components/HowTo/HowTo';
 
-export { Hero, Editorial };
+export { Hero, Editorial, HowTo };
