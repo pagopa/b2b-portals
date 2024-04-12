486e15f0b319b411a4617ac80589d6784f5c45c6
Meolocious <90854445+Meolocious@users.noreply.github.com>
2024-04-12 11:39:39 +0200

    commit remove unusued part

--- apps/nextjs-website/react-components/utils/index.ts
+++ apps/nextjs-website/react-components/utils/index.ts
@@ -1,6 +1,3 @@
-/* eslint-disable functional/no-expression-statements */
-/* eslint-disable functional/no-return-void */
-
 import React from 'react';
 import { type Generic } from '../types/components';
 
@@ -10,36 +7,3 @@ import { type Generic } from '../types/components';
  */
 export const isJSX = <T>(arg: T | Generic): arg is Generic =>
   React.isValidElement(arg);
-
-/**
- * a custom hook: it receives a ref as argument
- * and return a boolen with a true value when the ref pointing
- * to a tag element is visible
- */
-export function useIsVisible(ref: React.RefObject<Element>) {
-  const [isIntersecting, setIntersecting] = React.useState(false);
-
-  React.useEffect(() => {
-    const observer = new IntersectionObserver(([entry]) => {
-      if (entry) {
-        setIntersecting(entry.isIntersecting);
-      }
-    });
-
-    const observeCurrentRef = () => {
-      if (ref.current) {
-        observer.observe(ref.current);
-      }
-    };
-
-    observeCurrentRef();
-
-    return () => {
-      observer.disconnect();
-    };
-  }, [ref]);
-
-  return isIntersecting;
-}
-
-export const hrefNoOp = 'javascript:void(0)';
