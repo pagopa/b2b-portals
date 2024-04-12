49b0d21858a1d2336edfa0d32a91b96d074878fe
maurodandrea <98483756+maurodandrea@users.noreply.github.com>
2024-04-12 16:17:01 +0200

    terraform-fmt

--- apps/infrastructure/src/rds_aurora.tf
+++ apps/infrastructure/src/rds_aurora.tf
@@ -4,17 +4,16 @@ resource "aws_db_subnet_group" "cms_db_subnet" {
 }
 
 resource "aws_rds_cluster" "cms_database_cluster" {
-  cluster_identifier         = "cms-database-cluster"
-  engine                     = "aurora-postgresql"
-  engine_mode                = "provisioned"
-  engine_version             = "14"
-  auto_minor_version_upgrade = true
-  database_name              = "strapidb"
-  master_username            = "postgres"
-  master_password            = aws_ssm_parameter.cms_database_password.value
-  skip_final_snapshot        = true
-  vpc_security_group_ids     = [aws_security_group.cms_database.id]
-  db_subnet_group_name       = aws_db_subnet_group.cms_db_subnet.name
+  cluster_identifier     = "cms-database-cluster"
+  engine                 = "aurora-postgresql"
+  engine_mode            = "provisioned"
+  engine_version         = "14"
+  database_name          = "strapidb"
+  master_username        = "postgres"
+  master_password        = aws_ssm_parameter.cms_database_password.value
+  skip_final_snapshot    = true
+  vpc_security_group_ids = [aws_security_group.cms_database.id]
+  db_subnet_group_name   = aws_db_subnet_group.cms_db_subnet.name
 
   serverlessv2_scaling_configuration {
     max_capacity = 1.0
@@ -23,9 +22,10 @@ resource "aws_rds_cluster" "cms_database_cluster" {
 }
 
 resource "aws_rds_cluster_instance" "cms_database_instance" {
-  cluster_identifier   = aws_rds_cluster.cms_database_cluster.id
-  instance_class       = "db.serverless"
-  engine               = aws_rds_cluster.cms_database_cluster.engine
-  engine_version       = aws_rds_cluster.cms_database_cluster.engine_version
-  db_subnet_group_name = aws_db_subnet_group.cms_db_subnet.name
+  cluster_identifier         = aws_rds_cluster.cms_database_cluster.id
+  instance_class             = "db.serverless"
+  engine                     = aws_rds_cluster.cms_database_cluster.engine
+  engine_version             = aws_rds_cluster.cms_database_cluster.engine_version
+  auto_minor_version_upgrade = true
+  db_subnet_group_name       = aws_db_subnet_group.cms_db_subnet.name
 }
