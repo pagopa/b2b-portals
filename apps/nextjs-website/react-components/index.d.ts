49520358123ee2293352a68e7659e5a07c41d333
Meolocious <90854445+Meolocious@users.noreply.github.com>
2024-04-15 17:37:17 +0200

    commit module images

---
+++ apps/nextjs-website/react-components/index.d.ts
@@ -0,0 +1,26 @@
+declare module '*.jpg' {
+  const value: string;
+  export = value;
+}
+
+declare module '*.jpeg' {
+  const value: string;
+  export = value;
+}
+
+declare module '*.mp4' {
+  const value: string;
+  export = value;
+}
+
+declare module '*.png' {
+  const value: string;
+  export = value;
+}
+
+declare module '*.svg';
+
+declare module '*.pdf' {
+  const value: string;
+  export = value;
+}
