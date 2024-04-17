da99f38b310eb216b3e375cb6129543a720f48c4
Simone Salsi <59611877+simosalsi@users.noreply.github.com>
2024-04-17 11:48:01 +0200

    [B2BP-497] Set up Strapi data structure for multitenancy (#228)
    * Duplicated Strapi Single Types for SEND and AppIO tenants
    
    * Revert erroneous tsconfig change
    
    ---------
    
    Co-authored-by: Simone Salsi <simone.salsi@dgsspa.com>

---
+++ apps/strapi-cms/src/api/send-footer/controllers/send-footer.ts
@@ -0,0 +1,7 @@
+/**
+ * send-footer controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::send-footer.send-footer');
---
+++ apps/strapi-cms/src/api/send-footer/services/send-footer.ts
@@ -0,0 +1,7 @@
+/**
+ * send-footer service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::send-footer.send-footer');
