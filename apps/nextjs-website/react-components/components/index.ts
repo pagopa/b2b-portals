66b61ddaa48e03f7b9fd86102260425e8d6b11ac
Meolocious <90854445+Meolocious@users.noreply.github.com>
2024-04-19 11:34:44 +0200

    [B2BP-556] - Implementing 'Cards' EC inside B2BP (#253)
    * commit integrating Hero from EC into Next.JS
    
    * commit fix on Hero component
    
    * commit localhost for testing img in local
    
    * commit prettier config update
    
    * commit changeset
    
    * commit add comment to file
    
    * B2BP-427 modified hero layout
    
    * B2BP-427 modified hero layout
    
    * commit colour fix
    
    * commit remove localhost for testing image
    
    * commit editorial inside B2BP
    
    * commit refactoring editorial code
    
    * commit refactoring other editorial component files
    
    * commit update refactored files
    
    * commit implementing HowTo inside B2BP
    
    * commit implementing Feature EC inside B2BP
    
    * commit fix icon colour
    
    * commit fix install react-swipeable-views
    
    * commit shift folder into next.js instead of src/components
    
    * commit integration Accordion inside B2BP
    
    * commit fix colour
    
    * commit accordion grid fix
    
    * commit integration BannerLink inside B2BP
    
    * commit fix HowTo title colour
    
    * commit implementing Cards inside B2BP
    
    * Commit refactoring PR
    
    * Commit changeset
    
    * commit remove changes
    
    * commit remove changes
    
    * commit review changes
    
    * commit cards names change
    
    ---------
    
    Co-authored-by: salvatorediocaro <salvatore.diocaro@gmail.com>

--- apps/nextjs-website/react-components/components/index.ts
+++ apps/nextjs-website/react-components/components/index.ts
@@ -3,5 +3,6 @@ import Editorial from './Editorial/Editorial';
 import HowTo from './HowTo/HowTo';
 import Accordion from './Accordion/Accordion';
 import BannerLink from './BannerLink/BannerLink';
+import Cards from './Cards/Cards';
 
-export { Hero, Editorial, HowTo, Accordion, BannerLink };
+export { Hero, Editorial, HowTo, Accordion, BannerLink, Cards };
