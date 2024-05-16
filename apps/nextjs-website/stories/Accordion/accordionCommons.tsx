18e516e2c3822a4e9629a4810bde2ddaffcbae05
Meolocious <90854445+Meolocious@users.noreply.github.com>
2024-05-16 17:59:08 +0200

    commit remove unusued file

--- apps/nextjs-website/stories/Accordion/accordionCommons.tsx
+++
@@ -1,7 +0,0 @@
-const title = 'Lorem ipsum dolor sit amet, consectetur';
-const subtitle = `Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`;
-
-export const heroCommonProps = {
-  title,
-  subtitle,
-};
