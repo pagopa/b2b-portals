ada6bcfb838bf44d67e10c2b9a70d7250923524b
maurodandrea <98483756+maurodandrea@users.noreply.github.com>
2024-05-20 16:06:01 +0200

    Update apps/infrastructure/src/iam_policy.tf
    Co-authored-by: christian-calabrese <christian.calabrese@outlook.com>

--- apps/infrastructure/src/iam_policy.tf
+++ apps/infrastructure/src/iam_policy.tf
@@ -160,8 +160,8 @@ resource "aws_iam_policy" "deploy_website" {
         Effect = "Allow"
         Resource = concat(
           [aws_cloudfront_distribution.website.arn],
-          [for name, distribution in aws_cloudfront_distribution.cdn_multi_website : distribution.arn],
-          [aws_cloudfront_distribution.storybook.arn]
+          [aws_cloudfront_distribution.website.arn, aws_cloudfront_distribution.storybook.arn],
+          [for name, distribution in aws_cloudfront_distribution.cdn_multi_website : distribution.arn]
         )
       }
     ]
