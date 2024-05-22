7779a9d874c911c4a8de0a69fd0a9fe667526ac0
Meolocious <90854445+Meolocious@users.noreply.github.com>
2024-05-22 14:25:23 +0200

    commit update branch

--- apps/nextjs-website/stories/Feature/light.stories.tsx
+++ apps/nextjs-website/stories/Feature/light.stories.tsx
@@ -1,9 +1,6 @@
-// Import the necessary modules
-import { StoryFn, Meta } from '@storybook/react';
+import { Meta } from '@storybook/react';
 import { Feature } from '@react-components/components';
-import { FeatureProps } from '@react-components/types';
-import { FeatureItem } from '@react-components/types/Feature/Feature.types';
-
+import { FeatureTemplate, defaultPropsLightWithLinks, defaultPropsLightWithoutLinks } from './featureCommons';
 
 // Define the default export with metadata about your component
 export default {
@@ -11,63 +8,24 @@ export default {
   component: Feature,
 } as Meta;
 
-// Function to generate items
-const generateItemsWithLinks = (count: number): FeatureItem[] =>
-  Array.from({ length: count }, (_, i) => ({
-    title: `Feature ${i + 1}`,
-    subtitle: `This is feature ${i + 1}`,
-    stackIcon: {
-      icon: 'AccessAlarm',
-    },
-    link: {
-      text: `Link ${i + 1}`,
-      url: `https://example.com/link${i + 1}`,
-    },
-  })
-);
-
-// Function to generate items
-const generateItemsWithoutLinks = (count: number): FeatureItem[] =>
-  Array.from({ length: count }, (_, i) => ({
-    title: `Feature ${i + 1}`,
-    subtitle: `This is feature ${i + 1}`,
-    stackIcon: {
-      icon: 'AccessAlarm',
-    }
-  })
-);
-
-// Define a "Template" function that sets how args map to rendering
-const Template: StoryFn<FeatureProps> = (args) => <Feature {...args} />;
-
-// Define the default props
-const defaultProps: Partial<FeatureProps> = {
-  theme: 'light',
-  title: 'Feature Title',
-};
-
-export const LightFeatureFull = Template.bind({});
+export const LightFeatureFull = FeatureTemplate.bind({});
 LightFeatureFull.args = {
-  ...defaultProps,
-  items: generateItemsWithLinks(3),
+  ...defaultPropsLightWithLinks
 };
 
-export const LightFeatureNoLinks = Template.bind({});
+export const LightFeatureNoLinks = FeatureTemplate.bind({});
 LightFeatureNoLinks.args = {
-  ...defaultProps,
-  items: generateItemsWithoutLinks(3),
+  ...defaultPropsLightWithoutLinks
 };
 
-export const LightFeatureFullWithCarouselMobileOnly = Template.bind({});
+export const LightFeatureFullWithCarouselMobileOnly = FeatureTemplate.bind({});
 LightFeatureFullWithCarouselMobileOnly.args = {
-  ...defaultProps,
-  items: generateItemsWithLinks(3),
+  ...defaultPropsLightWithLinks,
   showCarouselMobile: true,
 };
 
-export const LightFeatureFullWithCustomBackground = Template.bind({});
+export const LightFeatureFullWithCustomBackground = FeatureTemplate.bind({});
 LightFeatureFullWithCustomBackground.args = {
-  ...defaultProps,
-  items: generateItemsWithLinks(3),
+  ...defaultPropsLightWithLinks,
   background: '#000',
 };
