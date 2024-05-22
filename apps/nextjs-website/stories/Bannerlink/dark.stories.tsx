c5a4902f57dd32217ebd3f8bcd065d980188d346
Meolocious <90854445+Meolocious@users.noreply.github.com>
2024-05-22 12:06:03 +0200

    [B2BP-592] - Storybook BannerLink (#277)
    * commit storybook
    
    * commit storybook fix
    
    * commit storybook
    
    * commit storybook fix
    
    * commit storybook fix
    
    * commit changeset
    
    * commit storybook update
    
    * commit storybook update
    
    * Fix path to the public folder
    
    * commit storybook update
    
    * commit storybook update
    
    * commit storybook update
    
    * remove assets
    
    * commit implementing Accordion inside storybook
    
    * commit changeset
    
    * commit implementing BannerLink inside storybook
    
    * commit changeset
    
    * commit update branch
    
    * Update lemon-toes-rescue.md
    
    * commit fix color name
    
    * commit filename change
    
    ---------
    
    Co-authored-by: Simone Salsi <simone.salsi@dgsspa.com>
    Co-authored-by: Carlo Alberto Degli Atti <lordkada@users.noreply.github.com>

---
+++ apps/nextjs-website/stories/Bannerlink/dark.stories.tsx
@@ -0,0 +1,20 @@
+import { Meta } from '@storybook/react';
+import { BannerLink } from '@react-components/components';
+import { BannerLinkTemplate, defaultPropsDark } from './bannerlinkCommons';
+
+// Define the default export with metadata about your component
+export default {
+  title: 'Components/BannerLink/Dark',
+  component: BannerLink,
+} as Meta;
+
+export const BannerLinkFull = BannerLinkTemplate.bind({});
+BannerLinkFull.args = {
+  ...defaultPropsDark,
+  body: 'This is a description for the banner link.',
+};
+
+export const BannerLinkOnlyTitle = BannerLinkTemplate.bind({});
+BannerLinkOnlyTitle.args = {
+  ...defaultPropsDark,
+};
