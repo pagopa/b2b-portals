83b70ac5deb88ce2a1be537db47df7dc3197b44e
Meolocious <90854445+Meolocious@users.noreply.github.com>
2024-05-22 12:26:28 +0200

    commit update branch

--- apps/nextjs-website/stories/Cards/dark.stories.tsx
+++ apps/nextjs-website/stories/Cards/dark.stories.tsx
@@ -1,8 +1,6 @@
-// Import the necessary modules
-import { StoryFn, Meta } from '@storybook/react';
+import { Meta } from '@storybook/react';
 import { Cards } from '@react-components/components';
-import { CardsProps } from '@react-components/types';
-import { CardsItemProps } from '@react-components/types/Cards/Cards.types';
+import { CardsTemplate, defaultPropsDarkThree, defaultPropsDarkThreeWithLinks, defaultPropsDarkFour, defaultPropsDarkFourWithLinks } from './cardsCommons';
 
 // Define the default export with metadata about your component
 export default {
@@ -10,83 +8,41 @@ export default {
   component: Cards,
 } as Meta;
 
-// Function to generate items
-const generateItems = (count: number): CardsItemProps[] =>
-  Array.from({ length: count }, (_, i) => ({
-    title: `Card ${i + 1}`,
-    text: `This is card ${i + 1}`,
-    cardIcon: {
-      icon: 'AccessAlarm',
-    }
-  })
-);
-
-// Function to generate items
-const generateItemsWithLinks = (count: number): CardsItemProps[] =>
-  Array.from({ length: count }, (_, i) => ({
-    title: `Card ${i + 1}`,
-    text: `This is card ${i + 1}`,
-    cardIcon: {
-      icon: 'AccessAlarm',
-    },
-    links: [
-      {
-        text: `Link ${i + 1}`,
-        href: '#',
-      },
-    ],
-  })
-);
-
-// Define a "Template" function that sets how args map to rendering
-const Template: StoryFn<CardsProps> = (args) => <Cards {...args} />;
-
-// Define the default props
-const defaultProps: Partial<CardsProps> = {
-  theme: 'dark',
-  items: generateItems(3),
-};
-
-export const DarkCardsThreeColumnOnlyTitleNoLinks = Template.bind({});
+export const DarkCardsThreeColumnOnlyTitleNoLinks = CardsTemplate.bind({});
 DarkCardsThreeColumnOnlyTitleNoLinks.args = {
-  ...defaultProps,
-  items: generateItems(3),
+  ...defaultPropsDarkThree,
   text: {
     title: 'Cards Title',
   },
 };
 
-export const DarkCardsThreeColumnOnlyTitleWithLinks = Template.bind({});
+export const DarkCardsThreeColumnOnlyTitleWithLinks = CardsTemplate.bind({});
 DarkCardsThreeColumnOnlyTitleWithLinks.args = {
-  ...defaultProps,
-  items: generateItemsWithLinks(3),
+  ...defaultPropsDarkThreeWithLinks,
   text: {
     title: 'Cards Title',
   },
 };
 
-export const DarkCardsFourColumnOnlyTitleNoLinks = Template.bind({});
+export const DarkCardsFourColumnOnlyTitleNoLinks = CardsTemplate.bind({});
 DarkCardsFourColumnOnlyTitleNoLinks.args = {
-  ...defaultProps,
-  items: generateItems(4),
+  ...defaultPropsDarkFour,
   text: {
     title: 'Cards Title',
   },
 };
 
-export const DarkCardsFourColumnOnlyTitleWithLinks = Template.bind({});
+export const DarkCardsFourColumnOnlyTitleWithLinks = CardsTemplate.bind({});
 DarkCardsFourColumnOnlyTitleWithLinks.args = {
-  ...defaultProps,
-  items: generateItemsWithLinks(4),
+  ...defaultPropsDarkFourWithLinks,
   text: {
     title: 'Cards Title',
   },
 };
 
-export const DarkCardsWithTextNoLinks = Template.bind({});
+export const DarkCardsWithTextNoLinks = CardsTemplate.bind({});
 DarkCardsWithTextNoLinks.args = {
-  ...defaultProps,
-  items: generateItems(4),
+  ...defaultPropsDarkFour,
   text: {
     title: 'Cards Title',
     subtitle: 'Cards Subtitle',
@@ -94,10 +50,9 @@ DarkCardsWithTextNoLinks.args = {
   },
 };
 
-export const DarkCardsWithTextWithLinks = Template.bind({});
+export const DarkCardsWithTextWithLinks = CardsTemplate.bind({});
 DarkCardsWithTextWithLinks.args = {
-  ...defaultProps,
-  items: generateItemsWithLinks(4),
+  ...defaultPropsDarkFourWithLinks,
   text: {
     title: 'Cards Title',
     subtitle: 'Cards Subtitle',
@@ -105,10 +60,9 @@ DarkCardsWithTextWithLinks.args = {
   },
 };
 
-export const DarkCardsWithTextandButtonNoLinks = Template.bind({});
+export const DarkCardsWithTextandButtonNoLinks = CardsTemplate.bind({});
 DarkCardsWithTextandButtonNoLinks.args = {
-  ...defaultProps,
-  items: generateItems(4),
+  ...defaultPropsDarkFour,
   text: {
     title: 'Cards Title',
     subtitle: 'Cards Subtitle',
@@ -122,10 +76,9 @@ DarkCardsWithTextandButtonNoLinks.args = {
   ],
 };
 
-export const DarkCardsWithTextandButtonWithLinks = Template.bind({});
+export const DarkCardsWithTextandButtonWithLinks = CardsTemplate.bind({});
 DarkCardsWithTextandButtonWithLinks.args = {
-  ...defaultProps,
-  items: generateItemsWithLinks(4),
+  ...defaultPropsDarkFourWithLinks,
   text: {
     title: 'Cards Title',
     subtitle: 'Cards Subtitle',
