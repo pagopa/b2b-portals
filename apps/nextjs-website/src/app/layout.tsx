34520850606bced2fb7b8cbda243fbc96c787934
Simone Salsi <simone.salsi@dgsspa.com>
2024-05-23 16:15:24 +0200

    Add Matomo Analytics

--- apps/nextjs-website/src/app/layout.tsx
+++ apps/nextjs-website/src/app/layout.tsx
@@ -4,7 +4,29 @@ import { theme } from './theme';
 import PreHeader from '@/components/PreHeader';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
-import { getPreHeaderProps, getHeaderProps, getFooterProps } from '@/lib/api';
+import {
+  getPreHeaderProps,
+  getHeaderProps,
+  getFooterProps,
+  getSiteWideSEO,
+} from '@/lib/api';
+
+const MatomoScript = (id: string): string => `
+var _paq = (window._paq = window._paq || []);
+/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+_paq.push(["trackPageView"]);
+_paq.push(["enableLinkTracking"]);
+(function () {
+  var u = "https://pagopa.matomo.cloud/";
+  _paq.push(["setTrackerUrl", u + "matomo.php"]);
+  _paq.push(["setSiteId", "${id}"]);
+  var d = document,
+    g = d.createElement("script"),
+    s = d.getElementsByTagName("script")[0];
+  g.async = true;
+  g.src = "//cdn.matomo.cloud/pagopa.matomo.cloud/matomo.js";
+  s.parentNode.insertBefore(g, s);
+})();`;
 
 export default async function RootLayout({
   children,
@@ -14,6 +36,7 @@ export default async function RootLayout({
   const preHeaderProps = await getPreHeaderProps();
   const headerProps = await getHeaderProps();
   const footerProps = await getFooterProps();
+  const { matomoID } = await getSiteWideSEO();
 
   return (
     <ThemeProvider theme={theme}>
@@ -29,6 +52,14 @@ export default async function RootLayout({
             id='otprivacy-notice-script'
             strategy='beforeInteractive'
           />
+          {matomoID !== null && (
+            <Script
+              id='matomo'
+              key='script-matomo'
+              dangerouslySetInnerHTML={{ __html: MatomoScript(matomoID) }}
+              strategy='lazyOnload'
+            />
+          )}
         </body>
       </html>
     </ThemeProvider>
