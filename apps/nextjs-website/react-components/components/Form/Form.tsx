9da0ab8baeb12b5116c1290ecf2ed422a941a20a
salvatorediocaro <salvatore.diocaro@gmail.com>
2024-06-27 11:21:09 +0200

    modified form

--- apps/nextjs-website/react-components/components/Form/Form.tsx
+++ apps/nextjs-website/react-components/components/Form/Form.tsx
@@ -9,24 +9,38 @@ import {
   FormControl,
   Divider,
   Button,
+  Checkbox,
 } from '@mui/material';
 import { FormData, FormProps } from '@react-components/types/Form/Form.types';
 import {
   TextColor,
   BackgroundColorAlternative,
-  LinkColor,
 } from '../common/Common.helpers';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
 
 const Form = (props: FormProps & { onSubmit: (data: FormData) => void }) => {
   const {
     title,
     subtitle,
-    privacyText,
-    privacyLink,
-    privacyLinkText,
+    privacyLinkRecaptchaPolicy,
+    privacyLinkTextRecaptchaPolicy,
+    privacyLinkRecaptchaTerms,
+    privacyLinkTextRecaptchaTerms,
     theme,
     backgroundImage,
     ctaButtons,
+    showFirstName = true,
+    showLastName = true,
+    showEmail = true,
+    showOrganization = true,
+    showCitizen = true,
+    showPublicEmployee = true,
+    showTechPartner = true,
+    showDeveloper = true,
+    showJournalist = true,
+    checkboxTitle,
+    showCheckboxInfo = true,
+    showPrivacyDisclaimer = true,
   } = props;
 
   const [formData, setFormData] = useState({
@@ -36,9 +50,16 @@ const Form = (props: FormProps & { onSubmit: (data: FormData) => void }) => {
     organization: '',
   });
 
+  const [checkboxes, setCheckboxes] = useState({
+    citizen: false,
+    publicEmployee: false,
+    techPartner: false,
+    developer: false,
+    journalist: false,
+  });
+
   const backgroundColor = BackgroundColorAlternative(theme);
   const textColor = TextColor(theme);
-  const linkColor = LinkColor(theme);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -48,8 +69,16 @@ const Form = (props: FormProps & { onSubmit: (data: FormData) => void }) => {
     }));
   };
 
+  const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, checked } = e.target;
+    setCheckboxes((prevData) => ({
+      ...prevData,
+      [name]: checked,
+    }));
+  };
+
   const handleButtonClick = () => {
-    props.onSubmit(formData);
+    props.onSubmit({ ...formData, ...checkboxes });
   };
 
   return (
@@ -88,6 +117,7 @@ const Form = (props: FormProps & { onSubmit: (data: FormData) => void }) => {
         },
       }}
     >
+      <MailOutlineIcon sx={{ fontSize: 50, mb: 2, color: textColor, zIndex: 3 }} />
       <Typography
         variant='h3'
         gutterBottom
@@ -107,71 +137,170 @@ const Form = (props: FormProps & { onSubmit: (data: FormData) => void }) => {
         spacing={2}
         sx={{ mb: 2, position: 'relative', zIndex: 3 }}
       >
-        <Grid item xs={12} sm={6}>
-          <FormControl fullWidth>
-            <OutlinedInput
-              placeholder='Nome'
-              name='firstName'
-              value={formData.firstName}
-              onChange={handleInputChange}
-              sx={{ backgroundColor: 'white', color: 'black' }}
-            />
-          </FormControl>
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <FormControl fullWidth>
-            <OutlinedInput
-              placeholder='Cognome'
-              name='lastName'
-              value={formData.lastName}
-              onChange={handleInputChange}
-              sx={{ backgroundColor: 'white', color: 'black' }}
-            />
-          </FormControl>
-        </Grid>
-        <Grid item xs={12}>
-          <FormControl fullWidth>
-            <OutlinedInput
-              placeholder='Indirizzo e-mail aziendale'
-              name='email'
-              value={formData.email}
-              onChange={handleInputChange}
-              sx={{ backgroundColor: 'white', color: 'black' }}
-            />
-          </FormControl>
-        </Grid>
-        <Grid item xs={12}>
-          <FormControl fullWidth>
-            <OutlinedInput
-              placeholder='Nome ente'
-              name='organization'
-              value={formData.organization}
-              onChange={handleInputChange}
-              sx={{ backgroundColor: 'white', color: 'black' }}
-            />
-          </FormControl>
-        </Grid>
+        {showFirstName && (
+          <Grid item xs={12} sm={6}>
+            <FormControl fullWidth>
+              <OutlinedInput
+                placeholder='Nome'
+                name='firstName'
+                value={formData.firstName}
+                onChange={handleInputChange}
+                sx={{ backgroundColor: 'white', color: 'black' }}
+              />
+            </FormControl>
+          </Grid>
+        )}
+        {showLastName && (
+          <Grid item xs={12} sm={6}>
+            <FormControl fullWidth>
+              <OutlinedInput
+                placeholder='Cognome'
+                name='lastName'
+                value={formData.lastName}
+                onChange={handleInputChange}
+                sx={{ backgroundColor: 'white', color: 'black' }}
+              />
+            </FormControl>
+          </Grid>
+        )}
+        {showEmail && (
+          <Grid item xs={12}>
+            <FormControl fullWidth>
+              <OutlinedInput
+                placeholder='Indirizzo e-mail aziendale'
+                name='email'
+                value={formData.email}
+                onChange={handleInputChange}
+                sx={{ backgroundColor: 'white', color: 'black' }}
+              />
+            </FormControl>
+          </Grid>
+        )}
+        {showOrganization && (
+          <Grid item xs={12}>
+            <FormControl fullWidth>
+              <OutlinedInput
+                placeholder='Nome ente'
+                name='organization'
+                value={formData.organization}
+                onChange={handleInputChange}
+                sx={{ backgroundColor: 'white', color: 'black' }}
+              />
+            </FormControl>
+          </Grid>
+        )}
       </Grid>
-      <Typography
-        variant='body2'
-        sx={{ mb: 2, position: 'relative', zIndex: 3, color: textColor }}
-      >
-        {privacyText}{' '}
-        <Link
-          href={privacyLink}
-          sx={{ color: linkColor, textDecorationColor: linkColor }}
+      {checkboxTitle && (
+        <Typography
+          variant='h5'
+          gutterBottom
+          sx={{ position: 'relative', zIndex: 3, mb: 2, color: textColor }}
         >
-          {privacyLinkText}
-        </Link>
-      </Typography>
-      <Divider
-        sx={{
-          mb: 2,
-          position: 'relative',
-          zIndex: 3,
-          backgroundColor: textColor,
-        }}
-      />
+          {checkboxTitle}
+        </Typography>
+      )}
+      <Grid
+        container
+        spacing={1}
+        sx={{ mb: 2, position: 'relative', zIndex: 3, textAlign: 'left' }}
+      >
+        {showCitizen && (
+          <Grid container alignItems='center' justifyContent='space-between'>
+            <Grid item xs>
+              <Typography variant='body1' fontWeight='bold' sx={{ color: textColor }}>
+                Cittadino
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Checkbox
+                checked={checkboxes.citizen}
+                onChange={handleCheckboxChange}
+                name='citizen'
+              />
+            </Grid>
+            <Divider sx={{ width: '100%' }} />
+          </Grid>
+        )}
+        {showPublicEmployee && (
+          <Grid container alignItems='center' justifyContent='space-between'>
+            <Grid item xs>
+              <Typography variant='body1' fontWeight='bold' sx={{ color: textColor }}>
+                Dipendente / Consulente di un ente pubblico
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Checkbox
+                checked={checkboxes.publicEmployee}
+                onChange={handleCheckboxChange}
+                name='publicEmployee'
+              />
+            </Grid>
+            <Divider sx={{ width: '100%' }} />
+          </Grid>
+        )}
+        {showTechPartner && (
+          <Grid container alignItems='center' justifyContent='space-between'>
+            <Grid item xs={10}>
+              <Typography variant='body1' fontWeight='bold' sx={{ color: textColor }}>
+                Partner tecnologico di un ente pubblico
+              </Typography>
+              <Typography variant='body2' sx={{ color: textColor }}>
+                (presso società in-house, software house, ecc.)
+              </Typography>
+            </Grid>
+            <Grid item xs={2} sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+              <Checkbox
+                checked={checkboxes.techPartner}
+                onChange={handleCheckboxChange}
+                name='techPartner'
+              />
+            </Grid>
+            <Divider sx={{ width: '100%' }} />
+          </Grid>
+        )}
+        {showDeveloper && (
+          <Grid container alignItems='center' justifyContent='space-between'>
+            <Grid item xs>
+              <Typography variant='body1' fontWeight='bold' sx={{ color: textColor }}>
+                Sviluppatore
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Checkbox
+                checked={checkboxes.developer}
+                onChange={handleCheckboxChange}
+                name='developer'
+              />
+            </Grid>
+            <Divider sx={{ width: '100%' }} />
+          </Grid>
+        )}
+        {showJournalist && (
+          <Grid container alignItems='center' justifyContent='space-between'>
+            <Grid item xs>
+              <Typography variant='body1' fontWeight='bold' sx={{ color: textColor }}>
+                Giornalista
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Checkbox
+                checked={checkboxes.journalist}
+                onChange={handleCheckboxChange}
+                name='journalist'
+              />
+            </Grid>
+            <Divider sx={{ width: '100%' }} />
+          </Grid>
+        )}
+      </Grid>
+      {showCheckboxInfo && (
+        <Typography
+          variant='body2'
+          sx={{ mb: 2, position: 'relative', zIndex: 3, color: textColor }}
+        >
+          *Campo obbligatorio, con possibilità di risposta multipla
+        </Typography>
+      )}
       {ctaButtons && ctaButtons.length > 0 && (
         <Button
           variant='contained'
@@ -181,6 +310,35 @@ const Form = (props: FormProps & { onSubmit: (data: FormData) => void }) => {
           {ctaButtons[0]?.text}
         </Button>
       )}
+      {showPrivacyDisclaimer && (
+        <Typography
+          variant='body2'
+          fontWeight='bold'
+          sx={{ mt: 2, position: 'relative', zIndex: 3, color: textColor }}
+        >
+          Inserendo il tuo indirizzo email stai accettando la nostra informativa sul trattamento dei dati personali per la newsletter.
+        </Typography>
+      )}
+      <Typography
+        variant='body2'
+        sx={{ mt: 2, position: 'relative', zIndex: 3, color: 'grey.600' }}
+      >
+        Form protetto tramite reCAPTCHA e Google{' '}
+        <Link
+          href={privacyLinkRecaptchaPolicy}
+          sx={{ color: 'grey.600', textDecorationColor: 'grey.600' }}
+        >
+          {privacyLinkTextRecaptchaPolicy}
+        </Link>{' '}
+        e{' '}
+        <Link
+          href={privacyLinkRecaptchaTerms}
+          sx={{ color: 'grey.600', textDecorationColor: 'grey.600' }}
+        >
+          {privacyLinkTextRecaptchaTerms}
+        </Link>{' '}
+        applicati.
+      </Typography>
     </Box>
   );
 };
--- apps/nextjs-website/react-components/types/Form/Form.types.ts
+++ apps/nextjs-website/react-components/types/Form/Form.types.ts
@@ -3,17 +3,39 @@ import { CtaButtonProps, type CommonProps } from '../common/Common.types';
 export interface FormProps extends CommonProps {
   readonly title: string;
   readonly subtitle: string;
-  readonly privacyText: string | JSX.Element;
-  readonly privacyLink: string;
-  readonly privacyLinkText: string;
+  readonly privacyTextRecaptcha: string;
+  readonly privacyLinkRecaptchaPolicy: string;
+  readonly privacyLinkTextRecaptchaPolicy: string;
+  readonly privacyLinkRecaptchaTerms: string;
+  readonly privacyLinkTextRecaptchaTerms: string;
   readonly theme: 'light' | 'dark';
   readonly backgroundImage?: string;
   readonly ctaButtons: ReadonlyArray<CtaButtonProps>;
+  readonly showFirstName?: boolean;
+  readonly showLastName?: boolean;
+  readonly showEmail?: boolean;
+  readonly showOrganization?: boolean;
+  readonly showCitizen?: boolean;
+  readonly showPublicEmployee?: boolean;
+  readonly showTechPartner?: boolean;
+  readonly showDeveloper?: boolean;
+  readonly showJournalist?: boolean;
+  readonly checkboxTitle?: string;
+  readonly showCheckboxInfo?: boolean;
+  readonly showPrivacyDisclaimer?: boolean;
+  readonly privacyDisclaimerText?: string;
 }
 
+
+
 export interface FormData {
-  firstName: string;
-  lastName: string;
+  firstName?: string;
+  lastName?: string;
   email: string;
-  organization: string;
-};
+  organization?: string;
+  citizen: boolean;
+  publicEmployee: boolean;
+  techPartner: boolean;
+  developer: boolean;
+  journalist: boolean;
+}
