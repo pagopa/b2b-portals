df3abeca2490f86954759cfabde200b19ed83fc5
Meolocious <90854445+Meolocious@users.noreply.github.com>
2024-07-05 10:35:59 +0200

    Update apps/nextjs-website/src/components/MegaHeader.tsx
    Co-authored-by: Simone Salsi <59611877+simosalsi@users.noreply.github.com>

--- apps/nextjs-website/src/components/MegaHeader.tsx
+++ apps/nextjs-website/src/components/MegaHeader.tsx
@@ -4,22 +4,8 @@ import MegaHeaderRC from '@react-components/components/MegaHeader/MegaHeader';
 import { MegaHeaderProps } from '@react-components/types/MegaHeader/MegaHeader.types';
 import { MegaHeaderSection } from '@/lib/fetch/types/PageSection';
 
-const makeMegaHeaderProps = ({
-  menuItems,
-  logoSrc,
-  logoAlt,
-  buttonHref,
-  ...rest
-}: MegaHeaderSection): MegaHeaderProps => ({
-  menuItems,
-  ...rest,
-  logoSrc,
-  logoAlt,
-  buttonHref,
-});
-
 const MegaHeader = (props: MegaHeaderSection) => (
-  <MegaHeaderRC {...makeMegaHeaderProps(props)} />
+  <MegaHeaderRC {...props} />
 );
 
 export default MegaHeader;
