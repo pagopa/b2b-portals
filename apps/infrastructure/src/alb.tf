4d81479737027157ea5c0aac5c9eae4b8ffb20cf
maurodandrea <98483756+maurodandrea@users.noreply.github.com>
2024-07-09 15:50:10 +0200

    re-add-target-group-old-cms

--- apps/infrastructure/src/alb.tf
+++ apps/infrastructure/src/alb.tf
@@ -5,6 +5,25 @@ resource "aws_alb" "cms_load_balancer" {
   internal        = false
 }
 
+### Remove this resource once the new multitenancy is complete
+resource "aws_alb_target_group" "cms" {
+  name        = "cms-target-group"
+  port        = var.cms_app_port
+  protocol    = "HTTP"
+  vpc_id      = module.vpc.vpc_id
+  target_type = "ip"
+
+  health_check {
+    healthy_threshold   = "3"
+    interval            = "30"
+    protocol            = "HTTP"
+    matcher             = "204"
+    timeout             = "3"
+    path                = "/_health"
+    unhealthy_threshold = "2"
+  }
+}
+
 # Redirect all traffic from the ALB to the target group
 resource "aws_alb_listener" "front_end" {
   load_balancer_arn = aws_alb.cms_load_balancer.id
