1fc7a8c780ac4e9f920b520bbe07cfb8f684c928
maurodandrea <98483756+maurodandrea@users.noreply.github.com>
2024-07-09 11:10:14 +0200

    remove-old-strapi-targetgroup

--- apps/infrastructure/src/alb.tf
+++ apps/infrastructure/src/alb.tf
@@ -5,25 +5,6 @@ resource "aws_alb" "cms_load_balancer" {
   internal        = false
 }
 
-### Remove this resource once the new multitenancy is complete
-resource "aws_alb_target_group" "cms" {
-  name        = "cms-target-group"
-  port        = var.cms_app_port
-  protocol    = "HTTP"
-  vpc_id      = module.vpc.vpc_id
-  target_type = "ip"
-
-  health_check {
-    healthy_threshold   = "3"
-    interval            = "30"
-    protocol            = "HTTP"
-    matcher             = "204"
-    timeout             = "3"
-    path                = "/_health"
-    unhealthy_threshold = "2"
-  }
-}
-
 # Redirect all traffic from the ALB to the target group
 resource "aws_alb_listener" "front_end" {
   load_balancer_arn = aws_alb.cms_load_balancer.id
@@ -49,8 +30,13 @@ resource "aws_lb_listener" "front_end_https" {
   certificate_arn   = module.acm.acm_certificate_arn
 
   default_action {
-    type             = "forward"
-    target_group_arn = aws_alb_target_group.cms.arn
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Invalid hostname"
+      status_code  = "401"
+    }
   }
 }
 
