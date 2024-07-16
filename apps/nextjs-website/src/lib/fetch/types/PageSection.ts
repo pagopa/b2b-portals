00667e2955fc2d4c888d387ad8854598dd86523d
Meolocious <90854445+Meolocious@users.noreply.github.com>
2024-07-16 18:06:58 +0200

    Commit merge fix

--- apps/nextjs-website/src/lib/fetch/types/PageSection.ts
+++ apps/nextjs-website/src/lib/fetch/types/PageSection.ts
@@ -120,7 +120,7 @@ const BannerLinkSectionCodec = t.strict({
       extraNormalText: t.union([t.string, t.null]),
       link: t.string,
       ctaButtons: t.array(CTAButtonSimpleCodec),
-      decoration: t.union([ImageDataCodec, t.type({ src: t.string, alt: t.union([t.string, t.undefined]) }), t.null]),
+      decoration: StrapiImageSchema,
       icon: t.union([FeatureItemMUIIconCodec, t.null]),
     })
   ),
