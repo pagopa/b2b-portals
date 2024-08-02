f71f372fdb5a529a6265683559ec551dfa7da89f
christian-calabrese <christian.calabrese@pagopa.it>
2024-08-02 09:34:33 +0200

    feat: created SNS topic for alarms notifications

---
+++ apps/infrastructure/src/sns.tf
@@ -0,0 +1,31 @@
+# SNS
+resource "aws_sns_topic" "alerts" {
+  name = "$b2bportals-${var.environment}-alerts"
+}
+
+resource "aws_sns_topic_policy" "alerts" {
+  arn = aws_sns_topic.alerts.arn
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowCloudWatchAlarms"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudwatch.amazonaws.com"
+        }
+        Action   = "sns:Publish"
+        Resource = aws_sns_topic.alerts.arn
+        Condition = {
+          ArnLike = {
+            "aws:SourceArn" = "arn:aws:cloudwatch:${var.aws_region}:${data.aws_caller_identity.current.account_id}:alarm:*"
+          }
+          StringEquals = {
+            "aws:SourceAccount" = data.aws_caller_identity.current.account_id
+          }
+        }
+      }
+    ]
+  })
+}
