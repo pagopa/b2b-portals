a138500d34de747b61720688f7923b2f32d0ff93
salvatorediocaro <salvatore.diocaro@gmail.com>
2024-08-08 04:05:40 +0200

    modified editorial

--- apps/nextjs-website/react-components/components/Editorial/Content.tsx
+++ apps/nextjs-website/react-components/components/Editorial/Content.tsx
@@ -1,10 +1,36 @@
 import React from 'react';
-import { useTheme } from '@mui/material';
+import { useTheme, Theme } from '@mui/material';
 import Stack from '@mui/material/Stack';
 import { EditorialContentProps } from '../../types/Editorial/Editorial.types';
 import { Body, Title } from '../common/Common';
 import { Eyelet } from './Editorial.helpers';
 import { TextColor, ExtraTextColor } from '../common/Common.helpers';
+
+const customStyles = (theme: Theme) => {
+  const { palette } = theme;
+  return {
+    dark: {
+      'a': {
+        color: `${palette.primary.contrastText} !important`,
+        fontWeight: '700 !important',
+        textDecorationColor: `${palette.primary.contrastText} !important`,
+        '&:hover': {
+          color: `${palette.primary.contrastText} !important`,
+        },
+      },
+    },
+    light: {
+      'a': {
+        color: `${palette.primary.main} !important`,
+        fontWeight: '700 !important',
+        textDecorationColor: `${palette.primary.main} !important`,
+        '&:hover': {
+          color: `${palette.primary.main} !important`,
+        },
+      },
+    },
+  };
+};
 
 export const Content = ({
   eyelet,
@@ -12,13 +38,14 @@ export const Content = ({
   body,
   theme,
 }: EditorialContentProps) => {
-  const { breakpoints } = useTheme();
+  const muiTheme = useTheme();
+  const { breakpoints } = muiTheme;
   const eyeletColor = ExtraTextColor(theme);
   const textColor = TextColor(theme);
   const maxTextWidth = breakpoints.values.md / 2;
 
   return (
-    <Stack maxWidth={{ md: maxTextWidth }} gap={2}>
+    <Stack maxWidth={{ md: maxTextWidth }} gap={2} sx={customStyles(muiTheme)[theme]}>
       {Eyelet(eyeletColor, eyelet)}
       <Title textColor={textColor} title={title} variant='h4' textAlign='left'/>
       <Body textColor={textColor} body={body} variant='body2'/>
