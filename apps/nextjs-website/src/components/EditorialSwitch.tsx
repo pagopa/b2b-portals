007e575c77f8e1bae9dbfd01ab16559f49bf4dcc
Meolocious <90854445+Meolocious@users.noreply.github.com>
2024-08-09 09:29:37 +0200

    commit changeset

--- apps/nextjs-website/src/components/EditorialSwitch.tsx
+++ apps/nextjs-website/src/components/EditorialSwitch.tsx
@@ -1,17 +1,21 @@
 'use client';
 import React from 'react';
 import { makeEditorialProps } from './Editorial';
+import MarkdownRenderer from './MarkdownRenderer';
 import { EditorialSwitch as EditorialSwitchRC } from '@react-components/components';
 import { EditorialSwitchProps } from '@react-components/types';
 import { EditorialSwitchSection } from '@/lib/fetch/types/PageSection';
-import MarkdownRenderer from './MarkdownRenderer';
 
 const makeEditorialSwitchProps = ({
   body,
   sections,
   ...rest
 }: EditorialSwitchSection): EditorialSwitchProps => ({
-  body: body ? (typeof body === 'string' ? MarkdownRenderer({ markdown: body, variant: 'body2' }) : body) : null,
+  body: body
+    ? typeof body === 'string'
+      ? MarkdownRenderer({ markdown: body, variant: 'body2' })
+      : body
+    : null,
   sections: sections.map(({ content, ...section }) => ({
     ...section,
     content: makeEditorialProps({
