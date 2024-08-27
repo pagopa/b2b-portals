d02d7eb9bbcbb8f4a209ed5a957383af78ef4ce4
Simone Salsi <simone.salsi@dgsspa.com>
2024-08-27 12:54:22 +0200

    Convert EditorialSwitch subtitle to Rich Text

--- apps/nextjs-website/src/components/EditorialSwitch.tsx
+++ apps/nextjs-website/src/components/EditorialSwitch.tsx
@@ -1,6 +1,7 @@
 'use client';
 import React from 'react';
 import { makeEditorialProps } from './Editorial';
+import MarkdownRenderer from './MarkdownRenderer';
 import { EditorialSwitch as EditorialSwitchRC } from '@react-components/components';
 import { EditorialSwitchProps } from '@react-components/types';
 import { EditorialSwitchSection } from '@/lib/fetch/types/PageSection';
@@ -10,7 +11,7 @@ const makeEditorialSwitchProps = ({
   sections,
   ...rest
 }: EditorialSwitchSection): EditorialSwitchProps => ({
-  ...(subtitle && { subtitle }),
+  ...(subtitle && { subtitle: MarkdownRenderer({ markdown: subtitle }) }),
   sections: sections.map(({ content, ...section }) => ({
     ...section,
     content: makeEditorialProps({
