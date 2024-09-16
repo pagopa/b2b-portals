7ebba8736cdc274a368c726dfb8f80d4275e28e0
salvatorediocaro <54940197+salvatorediocaro@users.noreply.github.com>
2024-09-16 11:04:53 +0200

    [B2BP-907] - Editorial: change image text order on mobile breakpoint (#435)
    * change image text order on mobile breakpoint
    
    * re-add comments
    
    * remove multiple rendering
    
    * change quotes

--- apps/nextjs-website/react-components/components/Editorial/Editorial.tsx
+++ apps/nextjs-website/react-components/components/Editorial/Editorial.tsx
@@ -41,7 +41,9 @@ const Editorial = (props: EditorialProps) => {
         size='xl'
         alignItems='center'
         background={backgroundColor}
-        direction={isMobile ? 'column' : reversed ? 'row-reverse' : 'row'}
+        direction={
+          isMobile ? 'column-reverse' : reversed ? 'row-reverse' : 'row'
+        }
         py={8}
         spacing={2}
         {...(sectionID && { sectionID })}
@@ -82,9 +84,7 @@ const Editorial = (props: EditorialProps) => {
   };
 
   const containerDirection = isMobile
-    ? reversed
-      ? 'column-reverse'
-      : 'column'
+    ? 'column-reverse'
     : reversed
       ? 'row-reverse'
       : 'row';
