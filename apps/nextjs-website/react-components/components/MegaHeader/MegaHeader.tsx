68934115e0185d57d493038133654fc72816c29f
Simone Salsi <simone.salsi@dgsspa.com>
2024-09-16 12:37:09 +0200

    Remove unused fragment

--- apps/nextjs-website/react-components/components/MegaHeader/MegaHeader.tsx
+++ apps/nextjs-website/react-components/components/MegaHeader/MegaHeader.tsx
@@ -60,203 +60,201 @@ const MegaHeader = (props: MegaHeaderProps) => {
   }, []);
 
   return (
-    <>
-      <Container>
-        <Content>
-          <Logo>
-            <img src={logoSrc} alt={logoAlt} />
-          </Logo>
-          {!isMobile && (
-            <>
-              <Nav>
-                {menuItems.map((menuItem: MegaMenuItem, index) => (
-                  <Typography component='li' key={index}>
-                    <a
-                      href='/'
-                      className={`menuPrimaryItem ${dropdownOpen === menuItem.primary ? 'active' : ''}`}
-                      onClick={(e) => handleClick(e, menuItem.primary)}
-                    >
-                      {menuItem.primary}
-                    </a>
-                  </Typography>
-                ))}
-              </Nav>
-              {ctaButton && (
-                <CtaButtons
-                  ctaButtons={[
-                    {
-                      ...ctaButton,
-                      sx: {
-                        backgroundColor: '#0B3EE3',
+    <Container>
+      <Content>
+        <Logo>
+          <img src={logoSrc} alt={logoAlt} />
+        </Logo>
+        {!isMobile && (
+          <>
+            <Nav>
+              {menuItems.map((menuItem: MegaMenuItem, index) => (
+                <Typography component='li' key={index}>
+                  <a
+                    href='/'
+                    className={`menuPrimaryItem ${dropdownOpen === menuItem.primary ? 'active' : ''}`}
+                    onClick={(e) => handleClick(e, menuItem.primary)}
+                  >
+                    {menuItem.primary}
+                  </a>
+                </Typography>
+              ))}
+            </Nav>
+            {ctaButton && (
+              <CtaButtons
+                ctaButtons={[
+                  {
+                    ...ctaButton,
+                    sx: {
+                      backgroundColor: '#0B3EE3',
+                      color: 'white',
+                      padding: '10px 20px',
+                      borderRadius: 2,
+                      textDecoration: 'none',
+                      whiteSpace: 'nowrap',
+                      '&:hover': {
+                        backgroundColor: '#005bb5',
+                      },
+                      '&:focus': {
+                        backgroundColor: '#005bb5',
+                      },
+                      '&:active': {
+                        backgroundColor: '#004999',
+                      },
+                      '&:visited': {
                         color: 'white',
-                        padding: '10px 20px',
-                        borderRadius: 2,
-                        textDecoration: 'none',
-                        whiteSpace: 'nowrap',
-                        '&:hover': {
-                          backgroundColor: '#005bb5',
-                        },
-                        '&:focus': {
-                          backgroundColor: '#005bb5',
-                        },
-                        '&:active': {
-                          backgroundColor: '#004999',
-                        },
-                        '&:visited': {
-                          color: 'white',
-                        },
                       },
                     },
-                  ]}
-                />
-              )}
-            </>
-          )}
-          <IconButton
-            className='hamburger'
-            onClick={handleMobileMenuToggle}
-            sx={{
-              display: { md: 'none' },
-              color: palette.custom.primaryColorDark,
-            }}
-          >
-            {mobileMenuOpen ? (
-              <CloseIcon style={{ color: palette.custom.primaryColorDark }} />
-            ) : (
-              <MenuIcon style={{ color: palette.custom.primaryColorDark }} />
+                  },
+                ]}
+              />
             )}
-          </IconButton>
-        </Content>
-
-        {!isMobile &&
-          menuItems.map((menuItem: MegaMenuItem, index) => (
-            <Dropdown
-              key={index}
-              className={dropdownOpen === menuItem.primary ? 'open' : ''}
-            >
-              {dropdownOpen === menuItem.primary &&
-                menuItem.secondary.map((submenu, subIndex) => (
-                  <div key={subIndex} className='dropdownSection'>
-                    <div style={{ display: 'flex', flexDirection: 'column' }}>
-                      {submenu.title && (
-                        <DropdownTitle>{submenu.title}</DropdownTitle>
-                      )}
-                      <div style={{ display: 'flex' }}>
-                        {Array.from(
-                          { length: Math.ceil(submenu.items.length / 7) },
-                          (_, colIndex) => (
-                            <Typography
-                              component='div'
-                              key={colIndex}
-                              className='column'
-                              style={{ flex: 1 }}
-                            >
-                              {submenu.items
-                                .slice(colIndex * 7, (colIndex + 1) * 7)
-                                .map((item, itemIndex) => (
-                                  <a
-                                    key={itemIndex}
-                                    href={item.href}
-                                    className={`menuSecondaryItem ${activeItem === item.label ? 'active' : ''}`}
-                                    onClick={() => setActiveItem(item.label)}
-                                  >
-                                    {item.label}
-                                    <ArrowForwardIcon className='arrowIcon' />
-                                  </a>
-                                ))}
-                            </Typography>
-                          )
-                        )}
-                      </div>
-                    </div>
-                  </div>
-                ))}
-            </Dropdown>
-          ))}
-
-        <MobileMenu
-          paddingTop={2}
-          id='mobileMenu'
-          className={mobileMenuOpen ? 'open' : ''}
+          </>
+        )}
+        <IconButton
+          className='hamburger'
+          onClick={handleMobileMenuToggle}
+          sx={{
+            display: { md: 'none' },
+            color: palette.custom.primaryColorDark,
+          }}
         >
-          {menuItems.map((menuItem: MegaMenuItem, index) => (
-            <React.Fragment key={index}>
-              <Stack
-                className={`mobileMenuPrimaryItem ${dropdownOpen === `mobile${menuItem.primary}` ? 'active' : ''}`}
-                onClick={(e) =>
-                  handleClick(e as any, `mobile${menuItem.primary}`)
-                }
-              >
-                <Typography>{menuItem.primary}</Typography>
-                <KeyboardArrowDownIcon
-                  style={{
-                    transform:
-                      dropdownOpen === `mobile${menuItem.primary}`
-                        ? 'rotate(180deg)'
-                        : 'rotate(0deg)',
-                    color: palette.custom.primaryColorDark,
-                  }}
-                />
-              </Stack>
-              <Stack
-                className={`dropdownMobile ${dropdownOpen === `mobile${menuItem.primary}` ? 'open' : ''}`}
-              >
-                {menuItem.secondary.map((submenu, subIndex) => (
-                  <div key={subIndex}>
+          {mobileMenuOpen ? (
+            <CloseIcon style={{ color: palette.custom.primaryColorDark }} />
+          ) : (
+            <MenuIcon style={{ color: palette.custom.primaryColorDark }} />
+          )}
+        </IconButton>
+      </Content>
+
+      {!isMobile &&
+        menuItems.map((menuItem: MegaMenuItem, index) => (
+          <Dropdown
+            key={index}
+            className={dropdownOpen === menuItem.primary ? 'open' : ''}
+          >
+            {dropdownOpen === menuItem.primary &&
+              menuItem.secondary.map((submenu, subIndex) => (
+                <div key={subIndex} className='dropdownSection'>
+                  <div style={{ display: 'flex', flexDirection: 'column' }}>
                     {submenu.title && (
                       <DropdownTitle>{submenu.title}</DropdownTitle>
                     )}
-                    {submenu.items.map((item, itemIndex) => (
-                      <a
-                        key={itemIndex}
-                        href={item.href}
-                        className={`mobileMenuSecondaryItem ${activeItem === item.label ? 'active' : ''}`}
-                        onClick={() => setActiveItem(item.label)}
-                      >
-                        <Typography variant='body2' fontSize={14}>
-                          {item.label}
-                        </Typography>
-                        <ArrowForwardIcon className='arrowIcon' />
-                      </a>
-                    ))}
+                    <div style={{ display: 'flex' }}>
+                      {Array.from(
+                        { length: Math.ceil(submenu.items.length / 7) },
+                        (_, colIndex) => (
+                          <Typography
+                            component='div'
+                            key={colIndex}
+                            className='column'
+                            style={{ flex: 1 }}
+                          >
+                            {submenu.items
+                              .slice(colIndex * 7, (colIndex + 1) * 7)
+                              .map((item, itemIndex) => (
+                                <a
+                                  key={itemIndex}
+                                  href={item.href}
+                                  className={`menuSecondaryItem ${activeItem === item.label ? 'active' : ''}`}
+                                  onClick={() => setActiveItem(item.label)}
+                                >
+                                  {item.label}
+                                  <ArrowForwardIcon className='arrowIcon' />
+                                </a>
+                              ))}
+                          </Typography>
+                        )
+                      )}
+                    </div>
                   </div>
-                ))}
-              </Stack>
-            </React.Fragment>
-          ))}
-          {ctaButton && (
-            <CtaButtons
-              ctaButtons={[
-                {
-                  ...ctaButton,
-                  sx: {
-                    backgroundColor: '#0B3EE3',
+                </div>
+              ))}
+          </Dropdown>
+        ))}
+
+      <MobileMenu
+        paddingTop={2}
+        id='mobileMenu'
+        className={mobileMenuOpen ? 'open' : ''}
+      >
+        {menuItems.map((menuItem: MegaMenuItem, index) => (
+          <React.Fragment key={index}>
+            <Stack
+              className={`mobileMenuPrimaryItem ${dropdownOpen === `mobile${menuItem.primary}` ? 'active' : ''}`}
+              onClick={(e) =>
+                handleClick(e as any, `mobile${menuItem.primary}`)
+              }
+            >
+              <Typography>{menuItem.primary}</Typography>
+              <KeyboardArrowDownIcon
+                style={{
+                  transform:
+                    dropdownOpen === `mobile${menuItem.primary}`
+                      ? 'rotate(180deg)'
+                      : 'rotate(0deg)',
+                  color: palette.custom.primaryColorDark,
+                }}
+              />
+            </Stack>
+            <Stack
+              className={`dropdownMobile ${dropdownOpen === `mobile${menuItem.primary}` ? 'open' : ''}`}
+            >
+              {menuItem.secondary.map((submenu, subIndex) => (
+                <div key={subIndex}>
+                  {submenu.title && (
+                    <DropdownTitle>{submenu.title}</DropdownTitle>
+                  )}
+                  {submenu.items.map((item, itemIndex) => (
+                    <a
+                      key={itemIndex}
+                      href={item.href}
+                      className={`mobileMenuSecondaryItem ${activeItem === item.label ? 'active' : ''}`}
+                      onClick={() => setActiveItem(item.label)}
+                    >
+                      <Typography variant='body2' fontSize={14}>
+                        {item.label}
+                      </Typography>
+                      <ArrowForwardIcon className='arrowIcon' />
+                    </a>
+                  ))}
+                </div>
+              ))}
+            </Stack>
+          </React.Fragment>
+        ))}
+        {ctaButton && (
+          <CtaButtons
+            ctaButtons={[
+              {
+                ...ctaButton,
+                sx: {
+                  backgroundColor: '#0B3EE3',
+                  color: 'white',
+                  padding: '10px 20px',
+                  borderRadius: 2,
+                  textDecoration: 'none',
+                  whiteSpace: 'nowrap',
+                  margin: '30px',
+                  '&:hover': {
+                    backgroundColor: '#005bb5',
+                  },
+                  '&:focus': {
+                    backgroundColor: '#005bb5',
+                  },
+                  '&:active': {
+                    backgroundColor: '#004999',
+                  },
+                  '&:visited': {
                     color: 'white',
-                    padding: '10px 20px',
-                    borderRadius: 2,
-                    textDecoration: 'none',
-                    whiteSpace: 'nowrap',
-                    margin: '30px',
-                    '&:hover': {
-                      backgroundColor: '#005bb5',
-                    },
-                    '&:focus': {
-                      backgroundColor: '#005bb5',
-                    },
-                    '&:active': {
-                      backgroundColor: '#004999',
-                    },
-                    '&:visited': {
-                      color: 'white',
-                    },
                   },
                 },
-              ]}
-            />
-          )}
-        </MobileMenu>
-      </Container>
-    </>
+              },
+            ]}
+          />
+        )}
+      </MobileMenu>
+    </Container>
   );
 };
 
