0ecffd073395f913be8379e4a0131458f348de3d
Simone Salsi <59611877+simosalsi@users.noreply.github.com>
2024-09-16 14:49:32 +0200

    Add types to HighlightBox stories (#439)
    Co-authored-by: Simone Salsi <simone.salsi@dgsspa.com>

--- apps/nextjs-website/stories/HighlightBox/default.stories.tsx
+++ apps/nextjs-website/stories/HighlightBox/default.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Meta, StoryFn } from '@storybook/react';
 import { HighlightBox } from '@react-components/components';
-import { HighlightBoxProps as HighlightBoxTypes } from '@react-components/types';
+import { HighlightBoxProps } from '@react-components/types';
 
 // Define the default export with metadata about your component
 const meta: Meta<typeof HighlightBox> = {
@@ -11,11 +11,11 @@ const meta: Meta<typeof HighlightBox> = {
 export default meta;
 
 // Define a 'Template' function for rendering
-const Template: StoryFn<HighlightBoxTypes> = (args) => (
+const Template: StoryFn<HighlightBoxProps> = (args) => (
   <HighlightBox {...args} />
 );
 
-export const HighlightBoxFull = Template.bind({});
+export const HighlightBoxFull: StoryFn<typeof HighlightBox> = Template.bind({});
 HighlightBoxFull.args = {
   title: 'Sta arrivando IT Wallet!',
   body: 'Se hai tra 18 e 35 anni compiuti puoi richiedere su IO la Carta Giovani Nazionale, istituita dal Dipartimento per le Politiche Giovanili e il Servizio Civile Universale. La carta dà diritto a sconti e agevolazioni per l’accesso ad attività culturali, sportive e ricreative, anche con finalità formative.',
@@ -29,7 +29,7 @@ HighlightBoxFull.args = {
   sectionID: 'highlight-box-1',
 };
 
-export const HighlightBoxWithoutButton = Template.bind({});
+export const HighlightBoxWithoutButton: StoryFn<typeof HighlightBox> = Template.bind({});
 HighlightBoxWithoutButton.args = {
   title: 'Sta arrivando IT Wallet!',
   body: 'Se hai tra 18 e 35 anni compiuti puoi richiedere su IO la Carta Giovani Nazionale, istituita dal Dipartimento per le Politiche Giovanili e il Servizio Civile Universale. La carta dà diritto a sconti e agevolazioni per l’accesso ad attività culturali, sportive e ricreative, anche con finalità formative.',
@@ -43,7 +43,7 @@ HighlightBoxWithoutButton.args = {
   sectionID: 'highlight-box-2',
 };
 
-export const HighlightBoxWithoutEyelet = Template.bind({});
+export const HighlightBoxWithoutEyelet: StoryFn<typeof HighlightBox> = Template.bind({});
 HighlightBoxWithoutEyelet.args = {
   title: 'Sta arrivando IT Wallet!',
   body: 'Se hai tra 18 e 35 anni compiuti puoi richiedere su IO la Carta Giovani Nazionale, istituita dal Dipartimento per le Politiche Giovanili e il Servizio Civile Universale. La carta dà diritto a sconti e agevolazioni per l’accesso ad attività culturali, sportive e ricreative, anche con finalità formative.',
