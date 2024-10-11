2a64a37362f65d63089da9d5ec302874f06ffb59
Simone Salsi <59611877+simosalsi@users.noreply.github.com>
2024-10-11 17:09:45 +0200

    [B2BP-917] Add PreFooter as site-wide component (#472)
    * Add PreFooter as site-wide component
    
    * Add exclude field to PreFooter
    
    ---------
    
    Co-authored-by: Simone Salsi <simone.salsi@dgsspa.com>

---
+++ apps/nextjs-website/src/lib/fetch/preFooter.ts
@@ -0,0 +1,60 @@
+import * as t from 'io-ts';
+import { extractFromResponse } from './extractFromResponse';
+import { extractTenantStrapiApiData } from './tenantApiData';
+import { ThemeCodec } from './types/Theme';
+import { StoreButtonsCodec } from './types/StoreButtons';
+import { StrapiImageSchema } from './types/StrapiImage';
+import { CTAButtonSimpleCodec } from './types/CTAButton';
+import { AppEnv } from '@/AppEnv';
+
+const PageRelationCodec = t.strict({
+  attributes: t.strict({
+    slug: t.string,
+  }),
+});
+
+const PreFooterAttributesCodec = t.strict({
+  title: t.string,
+  theme: ThemeCodec,
+  layout: t.keyof({
+    left: null,
+    center: null,
+  }),
+  background: StrapiImageSchema,
+  ctaButtons: t.array(CTAButtonSimpleCodec),
+  storeButtons: t.union([StoreButtonsCodec, t.null]),
+  exclude: t.strict({
+    data: t.array(PageRelationCodec),
+  }),
+});
+
+const PreFooterDataCodec = t.strict({
+  data: t.union([
+    t.null,
+    t.strict({
+      attributes: PreFooterAttributesCodec,
+    }),
+  ]),
+});
+
+type PreFooterData = t.TypeOf<typeof PreFooterDataCodec>;
+export type PreFooterAttributes = t.TypeOf<typeof PreFooterAttributesCodec>;
+
+export const getPreFooter = ({
+  config,
+  fetchFun,
+}: AppEnv): Promise<PreFooterData> =>
+  extractFromResponse(
+    fetchFun(
+      `${
+        extractTenantStrapiApiData(config).baseUrl
+      }/api/pre-footer/?populate=background,ctaButtons,storeButtons,exclude`,
+      {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${extractTenantStrapiApiData(config).token}`,
+        },
+      }
+    ),
+    PreFooterDataCodec
+  );
