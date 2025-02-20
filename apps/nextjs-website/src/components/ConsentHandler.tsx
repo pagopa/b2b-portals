60271bbf477784dd97f323098ab239773ba13a01
Simone Salsi <59611877+simosalsi@users.noreply.github.com>
2025-02-20 17:50:35 +0100

    [B2BP-1176] Allow footer links to show OneTrust Preferencies (#586)
    * Allow footer links to show OneTrust Preferencies
    
    * Remove obsolete OptanonActiveGroups declaration
    
    ---------
    
    Co-authored-by: Simone Salsi <simone.salsi@dgsspa.com>

--- apps/nextjs-website/src/components/ConsentHandler.tsx
+++ apps/nextjs-website/src/components/ConsentHandler.tsx
@@ -25,21 +25,27 @@ declare global {
   interface Window {
     // eslint-disable-next-line functional/no-return-void
     OptanonWrapper: () => void;
-    // OptanonActiveGroups will only be missing when CMS user inputs an invalid oneTrustDomainID
-    // We are only considering the case because we have no way to check beforehand if the id is valid
-    // Hence, this would cause a runtime error and break the built static website (which we want to avoid)
-    // Instead the cookie banner just won't show and an error will be logged to console
-    OptanonActiveGroups?: string;
     OneTrust: {
       // eslint-disable-next-line functional/no-return-void
       OnConsentChanged: (callback: () => void) => void;
+      // eslint-disable-next-line functional/no-return-void
+      ToggleInfoDisplay: () => void;
     };
   }
 }
 
 const targCookiesGroup = 'C0002'; // Target cookies (Mixpanel)
 
-const hasConsent = () => window.OptanonActiveGroups?.includes(targCookiesGroup);
+// Reading cookie instead of checking window.OneTrust.OptanonActiveGroups
+// because it will NEVER be instantiated at page load when we first check
+const hasConsent = () => {
+  const OTCookieValue: string =
+    document.cookie
+      .split('; ')
+      .find((row) => row.startsWith('OptanonConsent=')) || '';
+  const checkValue = `${targCookiesGroup}%3A1`;
+  return OTCookieValue.indexOf(checkValue) > -1;
+};
 
 const initMixpanel = (mixpanelConfig: NonNullable<MixpanelConfig>) =>
   mixpanel.init(mixpanelConfig.token, {
@@ -64,26 +70,31 @@ const ConsentHandler = ({
     initMixpanel(mixpanelConfig);
   }
 
-  useEffect(() => {
-    if (window.OptanonActiveGroups === undefined) {
-      console.error('ERROR: Invalid OneTrust domain ID');
-    }
-
-    // eslint-disable-next-line functional/immutable-data
-    window.OptanonWrapper = function () {
-      window.OneTrust.OnConsentChanged(function () {
-        if (hasConsent() && mixpanelConfig) {
+  const ToggleTracking = () => {
+    if (mixpanelConfig) {
+      if (hasConsent()) {
+        console.log('HAS CONSENT');
+        if (mixpanel.has_opted_out_tracking()) {
+          console.log('OPT IN');
           mixpanel.opt_in_tracking();
           // Track page view of page where consent is given
           mixpanel.track_pageview();
         }
-      });
+      } else if (mixpanel.has_opted_in_tracking()) {
+        console.log('OPT OUT');
+        mixpanel.opt_out_tracking();
+      }
+    }
+  };
+
+  useEffect(() => {
+    // eslint-disable-next-line functional/immutable-data
+    window.OptanonWrapper = function () {
+      window.OneTrust.OnConsentChanged(ToggleTracking);
     };
 
-    if (hasConsent() && mixpanelConfig) {
-      mixpanel.opt_in_tracking();
-    }
-  }, [mixpanelConfig]);
+    ToggleTracking();
+  }, [ToggleTracking]);
 
   return (
     <>
