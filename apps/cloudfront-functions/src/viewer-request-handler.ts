b24e13d2257319fcfb75255415f6d7ffe594963d
Carlo Alberto Degli Atti <lordkada@gmail.com>
2025-02-21 15:59:21 +0100

    Integrate the pawsbuilder-redirect logic

--- apps/cloudfront-functions/src/viewer-request-handler.ts
+++ apps/cloudfront-functions/src/viewer-request-handler.ts
@@ -1,14 +1,92 @@
+/* eslint-disable functional/prefer-readonly-type */
+/* eslint-disable functional/no-loop-statements */
+/* eslint-disable functional/no-let */
 // This code is executed in the CloudFront Functions JavaScript runtime. In this
 // context we prefer performance over immutability.
 // https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/writing-function-code.html#function-code-modify-request
 /* eslint-disable functional/immutable-data */
 /* eslint-disable functional/no-expression-statements */
 
+interface RegExp {
+  _helper?: string;
+}
+
+const simpleHelper = (base: string, usePath = true): RegExp => {
+  // eslint-disable-next-line functional/no-let
+  let stringRegex =
+    base == '' ? '.*' : base.replace(/\//g, '\\/').replace(/\./g, '\\.');
+  if (usePath) {
+    stringRegex += '(.*)';
+  }
+  const regex = new RegExp(stringRegex);
+  regex._helper = 'simpleHelper';
+  return regex;
+};
+
+interface RedirectRule {
+  readonly host: string;
+  readonly regex: RegExp;
+  readonly redirectTo: string;
+}
+
+const regexPatterns: readonly RedirectRule[] = [
+  {
+    host: 'ioapp.it',
+    regex: simpleHelper('/it/blocco-accesso/magic-link', false),
+    redirectTo: 'https://account.ioapp.it/it/blocco-accesso/link-scaduto/',
+  },
+  {
+    host: 'firma.io.italia.it',
+    regex: simpleHelper('/note-legali', false),
+    redirectTo: 'https://ioapp.it/firma-in-digitale/note-legali',
+  },
+  {
+    host: 'firma.io.italia.it',
+    regex: simpleHelper('/informativa-privacy', false),
+    redirectTo: 'https://ioapp.it/firma-in-digitale/informativa-privacy',
+  },
+  {
+    host: 'firma.io.italia.it',
+    regex: simpleHelper('', false),
+    redirectTo: 'https://ioapp.it/firma-in-digitale',
+  },
+];
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const handler = (
   event: AWSCloudFrontFunction.Event,
-): AWSCloudFrontFunction.Request => {
+): AWSCloudFrontFunction.Request | AWSCloudFrontFunction.Response => {
   if (event.context.eventType === 'viewer-request') {
+    const uri = event.request.uri;
+    const host = event.request.headers['host']?.value.toLocaleLowerCase();
+
+    for (const pattern of regexPatterns) {
+      if (pattern.host.toLocaleLowerCase() === host) {
+        const match = pattern.regex.exec(uri);
+
+        if (match) {
+          let path: string | null = null;
+
+          if (pattern.regex._helper === 'simpleHelper') {
+            path = match[1] || null;
+          }
+
+          let targetUri = pattern.redirectTo;
+          if (path !== null) {
+            targetUri += path;
+          }
+
+          return {
+            statusCode: 301,
+            statusDescription: 'Moved Permanently',
+            headers: {
+              location: { value: targetUri },
+            },
+          };
+        }
+      }
+    }
+
     // do the rewrite
     const { request } = event;
     const uriEndsWithSlash = request.uri.endsWith('/');
