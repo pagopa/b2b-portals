c6b150f9a1b2c910463b5861d8a595e42601121d
Simone Salsi <59611877+simosalsi@users.noreply.github.com>
2025-02-21 10:00:12 +0100

    [B2BP-1179] Initialize MixPanel only after consent has been given (#587)
    * Initialize MixPanel only after consent has been given
    
    * Remove console logs
    
    ---------
    
    Co-authored-by: Simone Salsi <simone.salsi@dgsspa.com>

--- apps/nextjs-website/src/components/TrackPageView.tsx
+++ apps/nextjs-website/src/components/TrackPageView.tsx
@@ -6,12 +6,12 @@ const TrackPageView = () => {
   useEffect(() => {
     // eslint-disable-next-line functional/no-try-statements
     try {
+      // Check opt in status to be sure
       if (mixpanel.has_opted_in_tracking()) {
         mixpanel.track_pageview();
       }
     } catch {
-      // This component should only be used if mixpanelConfig is defined
-      // Wrap calls in a try-catch anyway to prevent static website from breaking if the component is used incorrectly
+      // mixpanel throws an error if it's not initialized
     }
   }, []);
 
