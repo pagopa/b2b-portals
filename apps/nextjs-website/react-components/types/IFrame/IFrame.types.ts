0861b13fd2b7a375bd212b030d0e71cfa150b503
Simone Salsi <59611877+simosalsi@users.noreply.github.com>
2025-03-27 15:07:10 +0100

    Implement workaround for iframe-resizer's resize function not working (#618)
    Co-authored-by: Simone Salsi <simone.salsi@dgsspa.com>

--- apps/nextjs-website/react-components/types/IFrame/IFrame.types.ts
+++ apps/nextjs-website/react-components/types/IFrame/IFrame.types.ts
@@ -1,3 +1,14 @@
 export interface IFrameProps {
   src: string;
 }
+
+export interface IFrameResizerMessage {
+  message: {
+    type: string;
+    newChildHeight: number;
+  };
+}
+
+export interface IFrameResizerRef {
+  getElement: () => HTMLIFrameElement;
+}
