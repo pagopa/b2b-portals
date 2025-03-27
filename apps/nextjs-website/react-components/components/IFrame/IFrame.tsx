366379a8f5618ee8d71b83b8debcedb6b1d24ac4
Simone Salsi <simone.salsi@dgsspa.com>
2025-03-27 14:18:20 +0100

    Implement workaround for iframe-resizer's resize function not working

--- apps/nextjs-website/react-components/components/IFrame/IFrame.tsx
+++ apps/nextjs-website/react-components/components/IFrame/IFrame.tsx
@@ -1,13 +1,35 @@
-import React from 'react';
-import { IFrameProps } from '../../types/IFrame/IFrame.types';
+import React, { useRef } from 'react';
+import {
+  IFrameProps,
+  IFrameResizerRef,
+  IFrameResizerMessage,
+} from '../../types/IFrame/IFrame.types';
 import IframeResizer from '@iframe-resizer/react';
 
-const IFrame = ({ src }: IFrameProps) => (
-  <IframeResizer
-    license='GPLv3' // Open Source License
-    src={src}
-    style={{ width: '100%', height: '100vh', border: 'none' }}
-  />
-);
+const IFrame = ({ src }: IFrameProps) => {
+  const iframeRef = useRef<IFrameResizerRef>(null);
+
+  const handleMessage = ({
+    message: { type, newChildHeight },
+  }: IFrameResizerMessage) => {
+    if (type === 'resize' && newChildHeight && iframeRef.current) {
+      iframeRef.current.getElement().scrollIntoView({
+        behavior: 'smooth',
+      });
+      // Specific message created as a workaround when calling parentIFrame.resize() doesn't work
+      iframeRef.current.getElement().style.height = `${newChildHeight}px`;
+    }
+  };
+
+  return (
+    <IframeResizer
+      license='GPLv3' // Open Source License
+      src={src}
+      style={{ width: '100%', height: '100vh', border: 'none' }}
+      forwardRef={iframeRef}
+      onMessage={handleMessage}
+    />
+  );
+};
 
 export default IFrame;
