6ef5c32ec32ec219b3c145d72ac46a2c7a998dd8
salvatorediocaro <salvatore.diocaro@gmail.com>
2025-04-03 15:24:43 +0200

    modified script

--- apps/nextjs-website/react-components/components/DynamicsForm/DynamicsForm.tsx
+++ apps/nextjs-website/react-components/components/DynamicsForm/DynamicsForm.tsx
@@ -1,36 +1,25 @@
 'use client';
 
-import { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 import { Box } from '@mui/material';
+import Script from 'next/script';
 import { DynamicsFormProps } from '@react-components/types/DynamicsForm/DynamicsForm.types';
 
 const DynamicsForm = ({ formId, apiUrl, cachedUrl }: DynamicsFormProps) => {
-  const containerRef = useRef<HTMLDivElement>(null);
-
   useEffect(() => {
-    const script = document.createElement('script');
-    script.src =
-      'https://cxppusa1formui01cdnsa01-endpoint.azureedge.net/eur/FormLoader/FormLoader.bundle.js';
-    script.async = true;
     document.documentElement.lang = navigator.language;
-
-    const container = containerRef.current;
-    container?.appendChild(script);
-
-    return () => {
-      container?.removeChild(script);
-    };
   }, []);
 
   return (
     <Box sx={{ width: '100%', minHeight: '600px' }}>
-      <div ref={containerRef} style={{ width: '100%' }}>
-        <div
-          data-form-id={formId}
-          data-form-api-url={apiUrl}
-          data-cached-form-url={cachedUrl}
-        />
-      </div>
+      <div
+        id='dynamics-form-container'
+        data-form-id={formId}
+        data-form-api-url={apiUrl}
+        data-cached-form-url={cachedUrl}
+      />
+
+      <Script src='https://cxppusa1formui01cdnsa01-endpoint.azureedge.net/eur/FormLoader/FormLoader.bundle.js' />
     </Box>
   );
 };
