4cef1388bfe5dee464584037414312d2747e870a
Simone Salsi <simone.salsi@dgsspa.com>
2025-07-01 12:43:11 +0200

    Add sectionID to iFrame

--- apps/nextjs-website/react-components/types/IFrame/IFrame.types.ts
+++ apps/nextjs-website/react-components/types/IFrame/IFrame.types.ts
@@ -1,5 +1,6 @@
 export interface IFrameProps {
   src: string;
+  sectionID?: string;
 }
 
 export interface IFrameResizerMessage {
--- apps/nextjs-website/src/components/IFrame.tsx
+++ apps/nextjs-website/src/components/IFrame.tsx
@@ -1,7 +1,18 @@
 'use client';
 import { IFrame as IFrameRC } from '@react-components/components';
 import { IFrameSectionProps } from '@/lib/fetch/types/PageSection';
+import { IFrameProps } from '@react-components/types';
 
-const IFrameSection = (props: IFrameSectionProps) => <IFrameRC {...props} />;
+const makeIFrameProps = ({
+  src,
+  sectionID,
+}: IFrameSectionProps): IFrameProps => ({
+  src,
+  ...(sectionID && { sectionID }),
+});
+
+const IFrameSection = (props: IFrameSectionProps) => (
+  <IFrameRC {...makeIFrameProps(props)} />
+);
 
 export default IFrameSection;
