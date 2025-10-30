f26e729bb6695822afa04284aaabc2222dadc52f
Simone Salsi <simone.salsi@dgsspa.com>
2025-10-30 12:13:55 +0100

    Protect all pages behind access permissions

--- apps/strapi-cms/src/plugins/rollback/admin/src/pages/App.tsx
+++ apps/strapi-cms/src/plugins/rollback/admin/src/pages/App.tsx
@@ -1,14 +1,16 @@
 import { Page } from '@strapi/strapi/admin';
 import { Routes, Route } from 'react-router-dom';
-
 import { HomePage } from './HomePage';
+import pluginPermissions from '../permissions';
 
 const App = () => {
   return (
-    <Routes>
-      <Route index element={<HomePage />} />
-      <Route path="*" element={<Page.Error />} />
-    </Routes>
+    <Page.Protect permissions={pluginPermissions.access}>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="*" element={<Page.Error />} />
+      </Routes>
+    </Page.Protect>
   );
 };
 
