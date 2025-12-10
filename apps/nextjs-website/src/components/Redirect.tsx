0408d5b3c4408ced5c0a01e8a7ff9717226f5840
Stige79 <dario.scardazza@gmail.com>
2025-12-10 14:23:35 +0100

    Update apps/nextjs-website/src/app/[[...slug]]/page.tsx
    Co-authored-by: Simone Salsi <59611877+simosalsi@users.noreply.github.com>

--- apps/nextjs-website/src/components/Redirect.tsx
+++ apps/nextjs-website/src/components/Redirect.tsx
@@ -1,10 +1,12 @@
 'use client';
 
-import { useEffect } from 'react';
 import type { RedirectSection as RedirectProps } from '@/lib/fetch/types/PageSection';
 
 const Redirect = ({ redirectURL, redirectDelay }: RedirectProps) => (
-  <meta httpEquiv='refresh' content={`${Math.abs(redirectDelay)}; url=${redirectURL}`} />
+  <meta
+    httpEquiv='refresh'
+    content={`${Math.abs(redirectDelay)}; url=${redirectURL}`}
+  />
 );
 
 export default Redirect;
