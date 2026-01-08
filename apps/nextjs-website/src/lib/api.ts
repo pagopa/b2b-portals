2c3fd7b9998c96b113cd7c879a76aaa03c97c725
Simone Salsi <simone.salsi@dgsspa.com>
2026-01-08 16:29:52 +0100

    Remove unused isDryBuild method

--- apps/nextjs-website/src/lib/api.ts
+++ apps/nextjs-website/src/lib/api.ts
@@ -295,5 +295,3 @@ export const getPageSwitchPageDataFromID = async (
 export const isPreviewMode = () => appEnv.config.PREVIEW_MODE === 'true';
 
 export const getPreviewToken = () => appEnv.config.PREVIEW_TOKEN;
-
-export const isDryBuild = () => appEnv.config.DRY_BUILD === 'true';
