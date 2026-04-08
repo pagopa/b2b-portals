a7443424c098dc95757332d3fc2c6f9aafc6e999
uolter <walter.traspadini@pagopa.it>
2026-04-08 15:28:06 +0200

    Set cloudfront media library zero cache

--- apps/infrastructure/src/cloudfront.tf
+++ apps/infrastructure/src/cloudfront.tf
@@ -236,9 +236,9 @@ resource "aws_cloudfront_distribution" "cms_multitenant_medialibrary" {
     }
 
     viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 0     # min time for objects to live in the distribution cache
-    default_ttl            = 3600  # default time for objects to live in the distribution cache
-    max_ttl                = 86400 # max time for objects to live in the distribution cache
+    min_ttl                = 0 # no cache
+    default_ttl            = 0 # no cache
+    max_ttl                = 0 # no cache
   }
 
   restrictions {
