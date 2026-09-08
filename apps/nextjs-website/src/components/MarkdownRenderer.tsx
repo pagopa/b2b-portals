72a162a0fe99ae97828d3ad1cfa5dc6c0046698f
Stige79 <dario.scardazza@gmail.com>
2026-09-08 10:25:33 +0200

    [B2BP-1167] Open link external tab (#945)

--- apps/nextjs-website/src/components/MarkdownRenderer.tsx
+++ apps/nextjs-website/src/components/MarkdownRenderer.tsx
@@ -1,9 +1,16 @@
 import { marked } from 'marked';
-import parse from 'html-react-parser';
+import parse, {
+  attributesToProps,
+  DOMNode,
+  domToReact,
+  Element,
+  HTMLReactParserOptions,
+} from 'html-react-parser';
 import { SxProps, Theme, Typography } from '@mui/material';
 import { Variant } from '@mui/material/styles/createTypography';
 import { LocalizeMarkdownLinks } from '@/lib/linkLocalization';
 import { Locale } from '@/lib/fetch/siteWideSEO';
+import { isValidExternalLink } from '@react-components/components/common/Common';
 
 type MarkdownRendererProps = {
   markdown: string;
@@ -11,6 +18,27 @@ type MarkdownRendererProps = {
   defaultLocale: Locale;
   variant?: Variant;
   sx?: SxProps<Theme>;
+};
+
+const parserOptions: HTMLReactParserOptions = {
+  replace: (domNode) => {
+    if (
+      domNode instanceof Element &&
+      domNode.name === 'a' &&
+      isValidExternalLink(domNode.attribs['href'])
+    ) {
+      return (
+        <a
+          {...attributesToProps(domNode.attribs)}
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          {domToReact(domNode.children as DOMNode[], parserOptions)}
+        </a>
+      );
+    }
+    return undefined;
+  },
 };
 
 const MarkdownRenderer = ({
@@ -27,6 +55,7 @@ const MarkdownRenderer = ({
         async: false,
         breaks: true,
       }),
+      parserOptions,
     )}
   </Typography>
 );
