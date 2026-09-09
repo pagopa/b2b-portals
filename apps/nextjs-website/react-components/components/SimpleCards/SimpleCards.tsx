37881014486e3b7a778ef7c07394c5d5c17c9e7d
NicolaGiornetta <105795630+NicolaGiornetta@users.noreply.github.com>
2026-09-09 17:03:01 +0200

    [B2BP-1670] Fix SimpleCards image behaviour (#946)
    * Fix SimpleCards image behaviour
    
    * Fix image height and component width
    
    * Small fix
    
    * Fix CSS for body text

--- apps/nextjs-website/react-components/components/SimpleCards/SimpleCards.tsx
+++ apps/nextjs-website/react-components/components/SimpleCards/SimpleCards.tsx
@@ -1,4 +1,4 @@
-import { Box, Stack, Typography, useMediaQuery, useTheme } from '@mui/material';
+import { Box, Stack, Typography, useTheme } from '@mui/material';
 import ContainerRC from '../common/ContainerRC';
 import { CtaButtons, Title } from '../common/Common';
 import { TextColor } from '../common/Common.helpers';
@@ -23,7 +23,6 @@ const SimpleCards = ({
   const muiTheme = useTheme();
   const { palette } = muiTheme;
   const ctx = { palette, theme };
-  const isMobile = useMediaQuery(muiTheme.breakpoints.down('md'));
 
   const backgroundColor =
     customBgColor ??
@@ -53,10 +52,10 @@ const SimpleCards = ({
     <ContainerRC
       background={backgroundColor}
       py={8}
-      size='lg'
+      size='xl'
       sxInner={{
         width: '100%',
-        maxWidth: '1156px',
+        maxWidth: '1260px',
         mx: 'auto',
         px: 0,
         boxSizing: 'border-box',
@@ -79,7 +78,7 @@ const SimpleCards = ({
                   : 'flex-start',
             }}
             justifyContent='space-between'
-            spacing={{ xs: 4, md: shouldShowImage ? 8 : 0 }}
+            spacing={{ xs: 4, md: shouldShowImage ? 16 : 0 }}
             mb={6}
             color={textColor}
             textAlign={editorialTextAlign}
@@ -87,7 +86,8 @@ const SimpleCards = ({
             <Box
               sx={{
                 width: '100%',
-                maxWidth: { xs: '100%', md: shouldShowImage ? 448 : 684 },
+                flex: 1,
+                maxWidth: { xs: '100%', md: shouldShowImage ? 500 : 684 },
               }}
             >
               <Stack
@@ -187,23 +187,24 @@ const SimpleCards = ({
               <Box
                 sx={{
                   flex: 1,
-                  width: { xs: '100%', md: 315 },
+                  width: '100%',
+                  height: 'auto',
                   display: 'flex',
                   justifyContent: { xs: 'flex-start', md: 'flex-end' },
                   alignItems: 'center',
                   flexShrink: 0,
                   mt: { xs: 2, md: 0 },
-                  overflow: 'hidden',
                 }}
               >
                 <img
                   src={imageURL}
                   alt={imageAlt ?? ''}
                   style={{
-                    objectFit: 'cover',
-                    width: isMobile ? '100%' : 'auto',
-                    height: isMobile ? 'auto' : '315px',
-                    maxHeight: isMobile ? '490px' : '315px',
+                    objectFit: 'contain',
+                    objectPosition: 'center',
+                    width: '100%',
+                    height: 'auto',
+                    maxHeight: '490px',
                   }}
                 />
               </Box>
