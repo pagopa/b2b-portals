3a6e7976b1a7f6bade2b3af4b4a1bd8d40dc3a28
Stige79 <dario.scardazza@gmail.com>
2026-09-09 12:33:33 +0200

    [B2BP-1671] Fix LogoWall image sizing when a logo has no link (#948)
    * [B2BP-1671] Fix LogoWall image sizing when a logo has no link
    
    * [B2BP-1671] Remove redundant LogoWall width
    
    * [B2BP-1671] Remove redundant LogoWall width

--- apps/nextjs-website/react-components/components/LogoWall/LogoWall.tsx
+++ apps/nextjs-website/react-components/components/LogoWall/LogoWall.tsx
@@ -57,25 +57,23 @@ const LogoWall = ({
           minWidth: 0,
         }}
       >
-        {item.href ? (
-          <Link
-            href={item.href}
-            aria-label={item.title}
-            underline='none'
-            {...(isValidExternalLink(item.href) && {
+        <Link
+          underline='none'
+          {...(item.href && {
+            href: item.href,
+            'aria-label': item.title,
+            ...(isValidExternalLink(item.href) && {
               target: '_blank',
               rel: 'noopener noreferrer',
-            })}
-            sx={{
-              display: 'inline-flex',
-              maxWidth: '100%',
-            }}
-          >
-            {image}
-          </Link>
-        ) : (
-          image
-        )}
+            }),
+          })}
+          sx={{
+            display: 'inline-flex',
+            maxWidth: '100%',
+          }}
+        >
+          {image}
+        </Link>
       </Box>
     );
   };
